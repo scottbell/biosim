62a3c3fb200409576e17fd9526abc1808355791a
scott <scottbell@users.noreply.github.com>
2002-06-11 20:53:07 +0000

    *** empty log message ***

--- BIOSIM/bin/make-server.sh
+++ BIOSIM/bin/make-server.sh
@@ -17,7 +17,7 @@ then
 	echo "		-creating generated directory"
 fi
 separator=":"
-machineType=`echo "%OS%"`
+machineType=`uname`
 winName="Windows"
 case $machineType in
 	*$winName*) separator=";";echo "		-machine type is $winName";;
