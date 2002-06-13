cc59fe234b99ebd0c242562235d44c75a66679e2
scott <scott@4576cf88-1eed-0310-b270-a9f98363dfe6>
2002-06-13 15:44:10 +0000

    *** empty log message ***

--- trunk/BIOSIM/bin/run-client.sh
+++ trunk/BIOSIM/bin/run-client.sh
@@ -40,7 +40,7 @@ stubsClassesDir="$clientGenDir/stubs"
 clientDir="$devRootDir/src/biosim/client"
 controlName="biosim.client.control.BioSimulator"
 jacoClasspath="$JACORB_HOME/lib/jacorb.jar$separator$JRE_HOME/lib/rt.jar$separator$JACORB_HOME/lib"
-jacoInvocation="$java_command -classpath $clientClassesDir$separator$jacoClasspath$separator$CLASSPATH $jacoOrbClass $jacoSingletonOrbClass $jacoNameIOR"
+jacoInvocation="$java_command -classpath $clientClassesDir$separator$jacoClasspath $jacoOrbClass $jacoSingletonOrbClass $jacoNameIOR"
 echo "	-starting client"
 case $userSelect in
 	control) echo "			 -starting $userSelect";$jacoInvocation $controlName;;
