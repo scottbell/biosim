578920a86349b1ae463308ff7774905f92db4ed2
scott <scott@4576cf88-1eed-0310-b270-a9f98363dfe6>
2002-06-17 15:07:48 +0000

    *** empty log message ***

---
+++ trunk/BIOSIM/src/com/traclabs/biosim/client/framework/BiosimMain.java
@@ -0,0 +1,18 @@
+package biosim.client.control;
+
+import biosim.client.gui.*;
+import javax.swing.*;
+
+public class TestDriver
+{
+	public static void main(String args[])
+	{
+		BioSimulator biosim = new BioSimulator();
+		biosim.spawnSimulation();
+		SimPanel newPanel = new SimPanel();
+		JFrame theFrame = new JFrame("Advanced Life Support System");
+		theFrame.getContentPane().add(newPanel);
+		theFrame.setVisible(true);
+	}
+}
+
