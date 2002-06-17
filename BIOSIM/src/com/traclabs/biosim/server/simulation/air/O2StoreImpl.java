7e6acd99f4d6a7267100c539db0e7717bccdab6f
scott <scottbell@users.noreply.github.com>
2002-06-17 22:34:31 +0000

    *** empty log message ***

---
+++ BIOSIM/src/com/traclabs/biosim/server/simulation/air/O2StoreImpl.java
@@ -0,0 +1,46 @@
+package biosim.server.air;
+
+// The package containing our stubs.
+import SIMULATION.*;
+
+public class O2StoreImpl extends O2StorePOA {
+	private float O2level;
+	private float O2capacity;
+
+	float addO2(float liters){
+		if ((liters + O2level) > O2capacity){
+			O2Level = O2capacity;
+			if (liters >=  O2capacity)
+				return 0;
+			else
+				return (O2capacity - O2level);
+		}
+		else{
+			O2level = O2level + liters;
+			return liters;
+		}
+	}
+
+	float takeO2(float liters){
+		if ((O2level - liters) < 0){
+			O2Level = 0;
+			if (liters < 0)
+				return 0;
+			else
+				return O2level;
+		}
+		else{
+			O2level = O2level - liters;
+		}
+	}
+	float getO2StoreLevels(){
+		return O2level;
+	}
+
+	public void tick(){
+		System.out.println("O2Store has been ticked!");
+	}
+	public String getModuleName(){
+		return "O2Store";
+	}
+}
