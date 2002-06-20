2fd8ed88676a2d5fb64ea00bc508e91f83719e0b
scott <scottbell@users.noreply.github.com>
2002-06-20 17:11:24 +0000

    *** empty log message ***

--- BIOSIM/src/com/traclabs/biosim/server/simulation/air/AirRSImpl.java
+++ BIOSIM/src/com/traclabs/biosim/server/simulation/air/AirRSImpl.java
@@ -4,6 +4,22 @@ package biosim.server.air;
 import ALSS.*;
 
 public class AirRSImpl extends AirRSPOA {
+	private float currentO2Produced = 0f;
+	private float currentCO2Consumed = 0f;
+	private float currentPowerConsumed = 0f;
+
+	public  float getPowerConsumed(){
+		return currentPowerConsumed;
+	}
+
+	public  float getCO2Consumed(){
+		return currentCO2Consumed;
+	}
+
+	public  float getO2Produced(){
+		return currentO2Produced;
+	}
+
 	public void tick(){
 		System.out.println("AirRS has been ticked!");
 	}
--- BIOSIM/src/com/traclabs/biosim/server/simulation/food/FoodProcessorImpl.java
+++ BIOSIM/src/com/traclabs/biosim/server/simulation/food/FoodProcessorImpl.java
@@ -4,6 +4,22 @@ package biosim.server.food;
 import ALSS.*;
 
 public class FoodProcessorImpl extends FoodProcessorPOA {
+	private float currentBiomassConsumed = 0f;
+	private float currentPowerConsumed = 0f;
+	private float currentFoodProduced = 0f;
+
+	public float getBiomassConsumed(){
+		return currentBiomassConsumed;
+	}
+
+	public float getPowerConsumed(){
+		return currentPowerConsumed;
+	}
+
+	public float getFoodProduced(){
+		return currentFoodProduced;
+	}
+
 	public void tick(){
 		System.out.println("FoodProcessor has been ticked!");
 	}
