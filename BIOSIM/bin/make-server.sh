945d48ffa3a23a02562ab6f7eaa78a98f7551854
scott <scottbell@users.noreply.github.com>
2002-06-25 22:45:26 +0000

    *** empty log message ***

--- BIOSIM/bin/make-server.sh
+++ BIOSIM/bin/make-server.sh
@@ -18,7 +18,6 @@ then
 	java_command="java"
 	javac_command="javac"
 fi
-javaVersion=`$java_command -version`
 echo "java version is $javaVersion"
 genString="/generated"
 genDir=$devRootDir$genString
@@ -75,7 +74,7 @@ serverDir="$devRootDir/src/biosim/server"
 sourceDir="$devRootDir/src"
 jacoClasspath="$JACORB_HOME/lib/jacorb.jar$separator$JRE_HOME/lib/rt.jar$separator$JACORB_HOME/lib"
 compilationInvocation="$javac_command -d $serverClassesDir -classpath $skeletonDir$separator$serverClassesDir$separator$sourceDir$separator$jacoClasspath"
-echo "			-compiling skeletons with $compilationInvocation $simSkeletonDir/*.java"
+echo "			-compiling skeletons"
 $compilationInvocation $simSkeletonDir/*.java
 echo "			-compiling air"
 $compilationInvocation $serverDir/air/*.java
