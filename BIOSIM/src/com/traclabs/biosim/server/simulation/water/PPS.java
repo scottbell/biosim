b3a87faac09f1247fd122c160cff7321250a13c5
scott <scottbell@users.noreply.github.com>
2002-07-24 20:56:07 +0000

    added comments

--- BIOSIM/src/com/traclabs/biosim/server/simulation/water/PPS.java
+++ BIOSIM/src/com/traclabs/biosim/server/simulation/water/PPS.java
@@ -1,32 +1,28 @@
+/**
+ * The PPS is the last stage of water purification.  It takes water from the AES, filters it, and
+ * waits for the WaterRS to send the now clean water to the potable water store
+ *
+ * @author    Scott Bell
+ */
+
 package biosim.server.water;
 
 public class PPS extends WaterRSSubSystem{
-	private BWP myBWP;
-	private RO myRO;
-	private AES myAES;
-	private boolean hasCollectedReferences = false;
-	
+	/**
+	* Constructor that creates the PPS, the power required is 168 watts
+	* @param pWaterRSImpl The Water RS system the AES is contained in
+	*/
 	public PPS(WaterRSImpl pWaterRSImpl){
 		super(pWaterRSImpl);
 		powerNeeded =168; 
 	}
 	
+	/**
+	* Flushes the water from this subsystem (via the WaterRS) to the Potable Water Store
+	*/
 	public float takeWater(){
 		float potableWaterProduced = waterLevel;
 		waterLevel = 0;
 		return potableWaterProduced;
-	}
-	
-	private void collectReferences(){
-		if (!hasCollectedReferences){
-			myBWP = myWaterRS.getBWP();
-			myRO = myWaterRS.getRO();
-			myAES = myWaterRS.getAES();
-			hasCollectedReferences = true;
-		}
-	}
-
-	public void tick(){
-		collectReferences();
 	}
 }
--- BIOSIM/src/com/traclabs/biosim/server/simulation/water/RO.java
+++ BIOSIM/src/com/traclabs/biosim/server/simulation/water/RO.java
@@ -1,30 +1,50 @@
+/**
+ * The RO is the second stage of water purification.  It takes water from the BWP, filters it some, and
+ * sends the water to the AES
+ *
+ * @author    Scott Bell
+ */
+
 package biosim.server.water;
 
 public class RO extends WaterRSSubSystem{
-	private BWP myBWP;
+	//The subsystem to send the water to next
 	private AES myAES;
-	private PPS myPPS;
+	//Flag switched when the RO has collected references to other subsystems it needs
 	private boolean hasCollectedReferences = false;
 	
+	/**
+	* Constructor that creates the RO
+	* @param pWaterRSImpl The Water RS system the RO is contained in
+	*/
 	public RO(WaterRSImpl pWaterRSImpl){
 		super(pWaterRSImpl);
 	}
 	
+	/**
+	* Flushes the water from this subsystem to the AES
+	*/
 	private void pushWater(){
 		myWaterRS.getAES().addWater(waterLevel);
 		waterLevel = 0;
 	}
 	
+	/**
+	* In one tick, this subsystem:
+	* 1) Collects references (if needed)
+	* 2) Flushes the water from this subsystem to the AES
+	*/
 	public void tick(){
 		collectReferences();
 		pushWater();
 	}
 	
+	/**
+	* Collects references to subsystems needed for putting/getting resources
+	*/
 	private void collectReferences(){
 		if (!hasCollectedReferences){
-			myBWP = myWaterRS.getBWP();
 			myAES = myWaterRS.getAES();
-			myPPS = myWaterRS.getPPS();
 			hasCollectedReferences = true;
 		}
 	}
