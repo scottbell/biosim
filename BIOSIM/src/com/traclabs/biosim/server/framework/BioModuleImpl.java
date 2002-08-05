aa48a15ae3d17c5d58c45b17e954d5645a8d5b85
scott <scottbell@users.noreply.github.com>
2002-08-05 22:11:58 +0000

    *** empty log message ***

--- BIOSIM/src/com/traclabs/biosim/server/framework/BioModuleImpl.java
+++ BIOSIM/src/com/traclabs/biosim/server/framework/BioModuleImpl.java
@@ -13,6 +13,13 @@ public abstract class BioModuleImpl extends BioModulePOA{
 	*/
 	public void tick(){
 	}
+	
+	/**
+	* Logs this module to the Logger
+	*/
+	public void log(){
+	}
+	
 	/**
 	* Returns the name of the module, "Unamed" if not overriden
 	* @return the name of this module
