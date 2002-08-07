f554df6f8586dc138aab8345c698cff3a7ecf8e6
scott <scott@4576cf88-1eed-0310-b270-a9f98363dfe6>
2002-08-07 17:19:06 +0000

    *** empty log message ***

--- trunk/BIOSIM/src/com/traclabs/biosim/server/util/log/ScreenLogHandler.java
+++ trunk/BIOSIM/src/com/traclabs/biosim/server/util/log/ScreenLogHandler.java
@@ -8,45 +8,22 @@ import java.util.*;
  */
 
 public class ScreenLogHandler extends LogHandler{
+
 	public void writeLog(Log logToWrite){
-		printTree(logToWrite.getHead());
+		printTree(logToWrite.getHead(), 0);
 	}
 	//revert to recursive
-	private void printTree(LogNode rootNode) {
-		if (rootNode == null)
+	private void printTree(LogNode currentNode, int currentDepth) {
+		if (currentNode == null){
 			return;  // There is nothing to print in an empty tree.
-		Stack myNodeStack = new Stack();
-		Stack myChildCountStack = new Stack();
-		myNodeStack.push(rootNode);
-		while (!myNodeStack.isEmpty()){
-			LogNode poppedNode = (LogNode)(myNodeStack.pop());
-			LogNode[] childrenNodes =poppedNode.getChildren();
-			if (childrenNodes.length > 0){
-				myChildCountStack.push(new Integer(childrenNodes.length));
-			}
-			else{
-				myChildCountStack.push(new Integer(0));
-			}
-			for (int i = 0; i < myChildCountStack.size(); i++){
-				System.out.print("+");
-			}
-			System.out.println(poppedNode.getValue());
-			System.out.flush();
-			if (!myChildCountStack.isEmpty()){
-				Integer poppedCount = (Integer)(myChildCountStack.pop());
-				int newCount = poppedCount.intValue() - 1;
-				if (poppedCount.intValue() > 0)
-					myChildCountStack.push(new Integer(newCount));
-				if (poppedCount.intValue() < 0){
-					myChildCountStack.pop();
-					if (!myChildCountStack.isEmpty())
-						myChildCountStack.pop();
-				}
-			}
-			for (int i = 0; i < childrenNodes.length; i++){
-				myNodeStack.push(childrenNodes[i]);
-			}
 		}
-	} // end levelOrderPrint()
-
+		for (int i = 0; i < currentDepth; i ++){
+			System.out.print("\t");
+		}
+		System.out.println(currentNode.getValue());
+		LogNode[] childrenNodes =currentNode.getChildren();
+		for (int i = 0; i < childrenNodes.length; i++){
+			printTree(childrenNodes[i], currentDepth + 1);
+		}
+	}
 }
