f0196992fdeb0caf5d7ec832a765cfc77ddac0d8
scott <scottbell@users.noreply.github.com>
2002-08-28 22:41:45 +0000

    *** empty log message ***

---
+++ BIOSIM/src/com/traclabs/biosim/client/simulation/food/gui/FoodStorePanel.java
@@ -0,0 +1,160 @@
+package biosim.client.food.gui;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import biosim.idl.food.*;
+import biosim.client.framework.*;
+import com.jrefinery.chart.*;
+import com.jrefinery.data.*;
+import com.jrefinery.ui.*;
+
+/**
+ * This is the JPanel that displays a chart about the Food/Biomass Stores
+ *
+ * @author    Scott Bell
+ */
+public class FoodStorePanel extends JPanel
+{
+	private FoodStore myFoodStore;
+	private BiomassStore myBiomassStore;
+	private DefaultCategoryDataset myDataset;
+	private JButton refreshButton;
+	private JButton trackingButton;
+	private ChartPanel myChartPanel;
+	private RefreshAction myRefreshAction;
+	private TrackingAction myTrackingAction;
+	private Timer refreshTimer;
+	private final static int TIMER_DELAY=500;
+	private boolean trackingWanted = false;
+	private ValueAxis rangeAxis;
+	private CategoryPlot myPlot;
+	private JFreeChart myChart;
+
+	/**
+	 * Default constructor.
+	 */
+	public FoodStorePanel() {
+		myFoodStore = (FoodStore)(BioHolder.getBioModule(BioHolder.foodStoreName));
+		myBiomassStore = (BiomassStore)(BioHolder.getBioModule(BioHolder.biomassStoreName));
+		createGraph();
+		myRefreshAction = new RefreshAction("Refresh");
+		refreshButton = new JButton(myRefreshAction);
+		myTrackingAction = new TrackingAction("Start Tracking");
+		trackingButton = new JButton(myTrackingAction);
+		refreshTimer = new Timer (TIMER_DELAY, myRefreshAction);
+		
+		GridBagLayout gridbag = new GridBagLayout();
+		GridBagConstraints c = new GridBagConstraints();
+		setLayout(gridbag);
+		
+		c.fill = GridBagConstraints.BOTH;
+		c.gridheight = 1;
+		c.gridwidth = 1;
+		c.weighty = 0.1;
+		c.weightx = 0.1;
+		gridbag.setConstraints(refreshButton, c);
+		add(refreshButton);
+		
+		c.fill = GridBagConstraints.BOTH;
+		c.gridheight = 1;
+		c.gridwidth = GridBagConstraints.REMAINDER;
+		c.weighty = 0.1;
+		c.weightx = 0.1;
+		gridbag.setConstraints(trackingButton, c);
+		add(trackingButton);
+		
+		c.fill = GridBagConstraints.BOTH;
+		c.gridheight = 1;
+		c.gridwidth = GridBagConstraints.REMAINDER;
+		c.weighty = 1.0;
+		c.weightx = 1.0;
+		gridbag.setConstraints(myChartPanel, c);
+		add(myChartPanel);
+	}
+
+	private void createGraph(){
+		// create the chart...
+		refresh();
+		myChart = ChartFactory.createVerticalBarChart3D(
+		                  "Food Store Levels",  // chart title
+		                  "Stores",              // domain axis label
+		                  "Level (kg)",                 // range axis label
+		                  myDataset,                 // data
+		                  true                     // include legend
+		          );
+		// add the chart to a panel...
+		myPlot = myChart.getCategoryPlot();
+		rangeAxis = myPlot.getRangeAxis();
+		rangeAxis.setAutoRange(false);
+		rangeAxis.setRange(0.0, myFoodStore.getCapacity());
+		myPlot.setSeriesPaint(new Paint[] { new Color(51,153,51), new Color(204,204,0)});
+		TextTitle myTextTitle = (TextTitle)(myChart.getTitle(0));
+		myTextTitle.setFont(myTextTitle.getFont().deriveFont(12.0f));
+		myChartPanel = new ChartPanel(myChart);
+		myChartPanel.setMinimumDrawHeight(250);
+		myChartPanel.setMinimumDrawWidth(225);
+		myChartPanel.setPreferredSize(new Dimension(200, 200));
+	}
+
+	public void refresh() {
+		if (myDataset == null){
+			double[][] data = { {myBiomassStore.getLevel()}, {myFoodStore.getLevel()} };
+			myDataset = new DefaultCategoryDataset(data);
+			String[] theSeries = {"Biomass", "Food"};
+			String[] theCategory = {""};
+			myDataset.setSeriesNames(theSeries);
+			myDataset.setCategories(theCategory);
+		}
+		else{
+			if (rangeAxis.getRange().getUpperBound() != myFoodStore.getCapacity()){
+				rangeAxis.setRange(0.0, myFoodStore.getCapacity());
+				myChartPanel.repaint();
+			}
+			myDataset.setValue(0, "", new Float(myBiomassStore.getLevel()));
+			myDataset.setValue(1, "", new Float(myFoodStore.getLevel()));
+		}
+	}
+
+	public void visibilityChange(boolean nowVisible){
+		if (nowVisible && trackingWanted){
+			refreshTimer.start();
+		}
+		else{
+			refreshTimer.stop();
+		}
+	}
+
+	/**
+	* Action that displays the power panel in an internal frame on the desktop.
+	*/
+	private class RefreshAction extends AbstractAction{
+		public RefreshAction(String name){
+			super(name);
+		}
+		public void actionPerformed(ActionEvent ae){
+			refresh();
+		}
+	}
+
+	/**
+	* Action that displays the power panel in an internal frame on the desktop.
+	*/
+	private class TrackingAction extends AbstractAction{
+		public TrackingAction(String name){
+			super(name);
+		}
+		public void actionPerformed(ActionEvent ae){
+			if (refreshTimer.isRunning()){
+				refreshTimer.stop();
+				trackingButton.setText("Start Tracking");
+				trackingWanted = false;
+			}
+			else{
+				refreshTimer.start();
+				trackingButton.setText("Stop Tracking");
+				trackingWanted = true;
+			}
+		}
+	}
+}
