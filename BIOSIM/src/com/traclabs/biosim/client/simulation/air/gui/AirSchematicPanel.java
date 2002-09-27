df16c764770a57c8019543431dfc26cedc4aef6a
scott <scottbell@users.noreply.github.com>
2002-09-27 22:59:30 +0000

    *** empty log message ***

---
+++ BIOSIM/src/com/traclabs/biosim/client/simulation/air/gui/AirSchematicPanel.java
@@ -0,0 +1,23 @@
+package biosim.client.air.gui;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import biosim.idl.air.*;
+import biosim.client.framework.gui.*;
+
+/**
+ * This is the JPanel that displays a schematic
+ *
+ * @author    Scott Bell
+ */
+public class AirSchematicPanel extends BioTabPanel
+{
+
+	/**
+	 * Default constructor.
+	 */
+	public AirSchematicPanel() {
+	}
+
+}
