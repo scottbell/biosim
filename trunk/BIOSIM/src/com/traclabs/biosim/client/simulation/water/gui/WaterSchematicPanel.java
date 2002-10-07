d0772dd3e7e4eb9994e3a5fff949b4f04dc0a6c5
scott <scott@4576cf88-1eed-0310-b270-a9f98363dfe6>
2002-10-07 19:21:34 +0000

    *** empty log message ***

--- trunk/BIOSIM/src/com/traclabs/biosim/client/simulation/water/gui/WaterSchematicPanel.java
+++ trunk/BIOSIM/src/com/traclabs/biosim/client/simulation/water/gui/WaterSchematicPanel.java
@@ -14,16 +14,21 @@ import biosim.client.framework.*;
  */
 public class WaterSchematicPanel extends TimedPanel
 {
-	private ImageIcon problemIcon;
-	private ImageIcon offIcon;
-	private ImageIcon okIcon;
-	private JButton potableWaterButton;
-	private JButton greyWaterButton;
-	private JButton dirtyWaterButton;
+	private ImageIcon potableWaterStoreIcon;
+	private ImageIcon dirtyWaterStoreIcon;
+	private ImageIcon greyWaterStoreIcon;
+	private ImageIcon horizontalPipeIcon;
+	private ImageIcon susbsytemIcon;
 	private JButton AESButton;
 	private JButton BWPButton;
 	private JButton PPSButton;
 	private JButton ROButton;
+	private JLabel horizontalPipeLabel1;
+	private JLabel horizontalPipeLabel2;
+	private JLabel horizontalPipeLabel3;
+	private JLabel potableWaterLabel;
+	private JLabel greyWaterLabel;
+	private JLabel dirtyWaterLabel;
 	private WaterRS myWaterRS;
 	private PotableWaterStore myPotableWaterStore;
 	private GreyWaterStore myGreyWaterStore;
@@ -43,21 +48,138 @@ public class WaterSchematicPanel extends TimedPanel
 	
 	private void buildGui(){
 		loadIcons();
-		potableWaterButton = new JButton(new PotableWaterAction("Potable Water Store", okIcon));
-		greyWaterButton = new JButton(new GreyWaterAction("Grey Water Store", okIcon));
-		dirtyWaterButton = new JButton(new DirtyWaterAction("Dirty Water Store", okIcon));
-		AESButton = new JButton(new AESAction("AES Subsystem", okIcon));
-		BWPButton = new JButton(new BWPAction("BWP Subsystem", okIcon));
-		PPSButton = new JButton(new PPSAction("PPS Subsystem", okIcon));
-		ROButton = new JButton(new ROAction("RO Subsystem", okIcon));
-		setLayout(new GridLayout(7,1));
-		add(potableWaterButton);
-		add(greyWaterButton);
-		add(dirtyWaterButton);
-		add(AESButton);
+		AESButton = new JButton(new AESAction("AES (nominal)", susbsytemIcon));
+		AESButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+		AESButton.setHorizontalTextPosition(SwingConstants.CENTER);
+		BWPButton = new JButton(new BWPAction("BWP (nominal)", susbsytemIcon));
+		BWPButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+		BWPButton.setHorizontalTextPosition(SwingConstants.CENTER);
+		PPSButton = new JButton(new PPSAction("PPS (nominal)", susbsytemIcon));
+		PPSButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+		PPSButton.setHorizontalTextPosition(SwingConstants.CENTER);
+		ROButton = new JButton(new ROAction("RO (nominal)", susbsytemIcon));
+		ROButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+		ROButton.setHorizontalTextPosition(SwingConstants.CENTER);
+		horizontalPipeLabel1 = new JLabel(horizontalPipeIcon);
+		horizontalPipeLabel2 = new JLabel(horizontalPipeIcon);
+		horizontalPipeLabel3 = new JLabel(horizontalPipeIcon);
+		potableWaterLabel = new JLabel("Potable Water Store", potableWaterStoreIcon, SwingConstants.CENTER);
+		potableWaterLabel.setVerticalTextPosition(SwingConstants.BOTTOM);
+		potableWaterLabel.setHorizontalTextPosition(SwingConstants.CENTER);
+		dirtyWaterLabel = new JLabel("Dirty Water Store", dirtyWaterStoreIcon, SwingConstants.CENTER);
+		dirtyWaterLabel.setVerticalTextPosition(SwingConstants.BOTTOM);
+		dirtyWaterLabel.setHorizontalTextPosition(SwingConstants.CENTER);
+		greyWaterLabel = new JLabel("Grey Water Store", greyWaterStoreIcon, SwingConstants.CENTER);
+		greyWaterLabel.setVerticalTextPosition(SwingConstants.BOTTOM);
+		greyWaterLabel.setHorizontalTextPosition(SwingConstants.CENTER);
+		Color tempColor = this.getBackground();
+		System.out.println("red: "+tempColor.getRed());
+		System.out.println("green: "+tempColor.getGreen());
+		System.out.println("blue: "+tempColor.getBlue());
+		
+		//add components
+		GridBagLayout gridbag = new GridBagLayout();
+		GridBagConstraints c = new GridBagConstraints();
+		setLayout(gridbag);
+		c.fill = GridBagConstraints.NONE;
+		c.gridheight = 1;
+		c.weightx = 0.1;
+		c.gridwidth = 1;
+		c.weighty = 0.1;
+		c.ipadx = 0;
+		c.ipady = 0;
+		gridbag.setConstraints(BWPButton, c);
 		add(BWPButton);
-		add(PPSButton);
+
+		c.fill = GridBagConstraints.NONE;
+		c.gridheight = 1;
+		c.weightx = 0.1;
+		c.gridwidth = 1;
+		c.weighty = 0.1;
+		c.ipadx = 0;
+		c.ipady = 0;
+		gridbag.setConstraints(horizontalPipeLabel1, c);
+		add(horizontalPipeLabel1);
+
+		c.fill = GridBagConstraints.NONE;
+		c.gridheight = 1;
+		c.weightx = 0.1;
+		c.gridwidth = 1;
+		c.weighty = 0.1;
+		c.ipadx = 0;
+		c.ipady = 0;
+		gridbag.setConstraints(ROButton, c);
 		add(ROButton);
+		
+		c.fill = GridBagConstraints.NONE;
+		c.gridheight = 1;
+		c.weightx = 0.1;
+		c.gridwidth = 1;
+		c.weighty = 0.1;
+		c.ipadx = 0;
+		c.ipady = 0;
+		gridbag.setConstraints(horizontalPipeLabel2, c);
+		add(horizontalPipeLabel2);
+		
+		c.fill = GridBagConstraints.NONE;
+		c.gridheight = 1;
+		c.weightx = 0.1;
+		c.gridwidth = 1;
+		c.weighty = 0.1;
+		c.ipadx = 0;
+		c.ipady = 0;
+		gridbag.setConstraints(AESButton, c);
+		add(AESButton);
+		
+		c.fill = GridBagConstraints.NONE;
+		c.gridheight = 1;
+		c.weightx = 0.1;
+		c.gridwidth = 1;
+		c.weighty = 0.1;
+		c.ipadx = 0;
+		c.ipady = 0;
+		gridbag.setConstraints(horizontalPipeLabel3, c);
+		add(horizontalPipeLabel3);
+		
+		c.fill = GridBagConstraints.NONE;
+		c.gridheight = 1;
+		c.weightx = 0.1;
+		c.gridwidth = GridBagConstraints.REMAINDER;
+		c.weighty = 0.1;
+		c.ipadx = 0;
+		c.ipady = 0;
+		gridbag.setConstraints(PPSButton, c);
+		add(PPSButton);
+		
+		c.fill = GridBagConstraints.NONE;
+		c.gridheight = 1;
+		c.weightx = 0.1;
+		c.gridwidth = 1;
+		c.weighty = 0.1;
+		c.ipadx = 0;
+		c.ipady = 0;
+		gridbag.setConstraints(dirtyWaterLabel, c);
+		add(dirtyWaterLabel);
+		
+		c.fill = GridBagConstraints.NONE;
+		c.gridheight = 1;
+		c.weightx = 0.1;
+		c.gridwidth = 1;
+		c.weighty = 0.1;
+		c.ipadx = 0;
+		c.ipady = 0;
+		gridbag.setConstraints(greyWaterLabel, c);
+		add(greyWaterLabel);
+		
+		c.fill = GridBagConstraints.NONE;
+		c.gridheight = 1;
+		c.weightx = 0.1;
+		c.gridwidth = 1;
+		c.weighty = 0.1;
+		c.ipadx = 0;
+		c.ipady = 0;
+		gridbag.setConstraints(potableWaterLabel, c);
+		add(potableWaterLabel);
 	}
 	
 	public void refresh(){
@@ -68,15 +190,20 @@ public class WaterSchematicPanel extends TimedPanel
 	*/
 	private void loadIcons(){
 		try{
-			problemIcon = new ImageIcon(ClassLoader.getSystemClassLoader().getResource("biosim/client/water/gui/problemIcon.gif"));
-			offIcon = new ImageIcon(ClassLoader.getSystemClassLoader().getResource("biosim/client/water/gui/offIcon.gif"));
-			okIcon = new ImageIcon(ClassLoader.getSystemClassLoader().getResource("biosim/client/water/gui/okIcon.gif"));
+			potableWaterStoreIcon = new ImageIcon(ClassLoader.getSystemClassLoader().getResource("biosim/client/water/gui/potable-watertank.jpg"));
+			dirtyWaterStoreIcon = new ImageIcon(ClassLoader.getSystemClassLoader().getResource("biosim/client/water/gui/dirty-watertank.jpg"));
+			greyWaterStoreIcon = new ImageIcon(ClassLoader.getSystemClassLoader().getResource("biosim/client/water/gui/grey-watertank.jpg"));
+			horizontalPipeIcon = new ImageIcon(ClassLoader.getSystemClassLoader().getResource("biosim/client/water/gui/horizontal-pipe.jpg"));
+			susbsytemIcon = new ImageIcon(ClassLoader.getSystemClassLoader().getResource("biosim/client/water/gui/subsystem.jpg"));
 		}
 		catch (Exception e){
-			System.out.println("Couldn't find icon ("+e+"), skipping");
-			problemIcon = new ImageIcon();
-			offIcon = new ImageIcon();
-			okIcon = new ImageIcon();
+			System.out.println("Couldn't find icon, skipping icon loading");
+			e.printStackTrace();
+			potableWaterStoreIcon = new ImageIcon();
+			dirtyWaterStoreIcon = new ImageIcon();
+			greyWaterStoreIcon = new ImageIcon();
+			horizontalPipeIcon = new ImageIcon();
+			susbsytemIcon = new ImageIcon();
 		}
 	}
 	
@@ -118,9 +245,9 @@ public class WaterSchematicPanel extends TimedPanel
 			setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
 			myWaterRS.setAESEnabled(!myWaterRS.AESIsEnabled());
 			if (myWaterRS.AESIsEnabled())
-				AESButton.setIcon(okIcon);
+				AESButton.setText("AES (nominal)");
 			else if (!myWaterRS.AESIsEnabled())
-				AESButton.setIcon(offIcon);
+				AESButton.setText("AES (off)");
 			setCursor(Cursor.getDefaultCursor());
 		}
 	}
@@ -153,9 +280,9 @@ public class WaterSchematicPanel extends TimedPanel
 			setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
 			myWaterRS.setROEnabled(!myWaterRS.ROIsEnabled());
 			if (myWaterRS.ROIsEnabled())
-				ROButton.setIcon(okIcon);
+				ROButton.setText("RO (nominal)");
 			else if (!myWaterRS.ROIsEnabled())
-				ROButton.setIcon(offIcon);
+				ROButton.setText("RO (off)");
 			setCursor(Cursor.getDefaultCursor());
 		}
 	}
