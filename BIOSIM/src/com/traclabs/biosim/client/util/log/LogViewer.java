a4086cff4342da407edd77e4edc738f743558218
scott <scottbell@users.noreply.github.com>
2002-10-23 20:49:47 +0000

    *** empty log message ***

--- BIOSIM/src/com/traclabs/biosim/client/util/log/LogViewer.java
+++ BIOSIM/src/com/traclabs/biosim/client/util/log/LogViewer.java
@@ -1,6 +1,7 @@
 package biosim.client.util.log;
 
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.tree.*;
 import biosim.idl.util.*;
@@ -22,33 +23,73 @@ public class LogViewer extends JPanel
 	private ImageIcon backIcon;
 	private ImageIcon nextIcon;
 	private ImageIcon firstIcon;
-	
+	private OpenAction myOpenAction;
+	private RefreshAction myRefreshAction;
+	private LastAction myLastAction;
+	private BackAction myBackAction;
+	private NextAction myNextAction;
+	private FirstAction myFirstAction;
+	private JButton openButton;
+	private JButton refreshButton;
+	private JButton lastButton;
+	private JButton backButton;
+	private JButton nextButton;
+	private JButton firstButton;
+
 	/**
 	* Default constructor
 	*/
 	public LogViewer(){
 		buildGui();
 	}
-	
+
 	private void loadIcons(){
-		folderIcon = new ImageIcon(ClassLoader.getSystemClassLoader().getResource("biosim/client/util/log/folder.jpg"));
-		refreshIcon = new ImageIcon(ClassLoader.getSystemClassLoader().getResource("biosim/client/util/log/refresh.jpg"));
-		lastIcon = new ImageIcon(ClassLoader.getSystemClassLoader().getResource("biosim/client/util/log/last.jpg"));
-		backIcon = new ImageIcon(ClassLoader.getSystemClassLoader().getResource("biosim/client/util/log/back.jpg"));
-		nextIcon = new ImageIcon(ClassLoader.getSystemClassLoader().getResource("biosim/client/util/log/next.jpg"));
-		firstIcon = new ImageIcon(ClassLoader.getSystemClassLoader().getResource("biosim/client/util/log/first.jpg"));
+		try{
+			folderIcon = new ImageIcon(ClassLoader.getSystemClassLoader().getResource("biosim/client/util/log/folder.gif"));
+			refreshIcon = new ImageIcon(ClassLoader.getSystemClassLoader().getResource("biosim/client/util/log/refresh.gif"));
+			backIcon = new ImageIcon(ClassLoader.getSystemClassLoader().getResource("biosim/client/util/log/back.gif"));
+			nextIcon = new ImageIcon(ClassLoader.getSystemClassLoader().getResource("biosim/client/util/log/next.gif"));
+			lastIcon = new ImageIcon(ClassLoader.getSystemClassLoader().getResource("biosim/client/util/log/last.gif"));
+			firstIcon = new ImageIcon(ClassLoader.getSystemClassLoader().getResource("biosim/client/util/log/first.gif"));
+		}
+		catch(Exception e){
+			System.out.println("Couldn't load icons!");
+			e.printStackTrace();
+		}
 	}
-	
+
 	/**
 	* Creates close listener and adds it to the window
 	*/
 	private void buildGui(){
+		loadIcons();
+		setLayout(new BorderLayout());
 		myTopNode = new DefaultMutableTreeNode("Log");
 		myLogTree = new JTree(myTopNode);
 		myToolBar = new JToolBar();
-		add(myLogTree);
+		myToolBar.setFloatable(false);
+		myOpenAction = new OpenAction("Open", folderIcon);
+		myRefreshAction = new RefreshAction("Refresh", refreshIcon);
+		myLastAction = new LastAction("Last", lastIcon);
+		myBackAction = new BackAction("Back", backIcon);
+		myNextAction = new NextAction("Next", nextIcon);
+		myFirstAction = new FirstAction("First", firstIcon);
+		openButton = myToolBar.add(myOpenAction);
+		openButton.setText("");
+		refreshButton = myToolBar.add(myRefreshAction);
+		refreshButton.setText("");
+		lastButton = myToolBar.add(myLastAction);
+		lastButton.setText("");
+		backButton = myToolBar.add(myBackAction);
+		backButton.setText("");
+		nextButton = myToolBar.add(myNextAction);
+		nextButton.setText("");
+		firstButton = myToolBar.add(myFirstAction);
+		firstButton.setText("");
+		add(myToolBar, BorderLayout.NORTH);
+		add(myLogTree, BorderLayout.CENTER);
 	}
-	
+
 	public static void main(String args[]){
 		LogViewer myLogViewer = new LogViewer();
 		BioFrame myFrame = new BioFrame("Log Viewer", false);
@@ -58,6 +99,89 @@ public class LogViewer extends JPanel
 		myFrame.setVisible(true);
 	}
 
+	/**
+	* Action that opens a file for input
+	*/
+	private class OpenAction extends AbstractAction{
+		public OpenAction(String name, Icon icon){
+			super(name, icon);
+		}
+		public void actionPerformed(ActionEvent ae){
+			setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+			System.out.println("Open!");
+			setCursor(Cursor.getDefaultCursor());
+		}
+	}
+
+	/**
+	* Action that refresh a file for input
+	*/
+	private class RefreshAction extends AbstractAction{
+		public RefreshAction(String name, Icon icon){
+			super(name, icon);
+		}
+		public void actionPerformed(ActionEvent ae){
+			setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+			System.out.println("Refresh!");
+			setCursor(Cursor.getDefaultCursor());
+		}
+	}
+
+	/**
+	* Action that goes back to the last timestep
+	*/
+	private class LastAction extends AbstractAction{
+		public LastAction(String name, Icon icon){
+			super(name, icon);
+		}
+		public void actionPerformed(ActionEvent ae){
+			setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+			System.out.println("Last!");
+			setCursor(Cursor.getDefaultCursor());
+		}
+	}
+
+	/**
+	* Action that goes back one timestep
+	*/
+	private class BackAction extends AbstractAction{
+		public BackAction(String name, Icon icon){
+			super(name, icon);
+		}
+		public void actionPerformed(ActionEvent ae){
+			setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+			System.out.println("Back!");
+			setCursor(Cursor.getDefaultCursor());
+		}
+	}
+
+	/**
+	* Action that goes up one timestep
+	*/
+	private class NextAction extends AbstractAction{
+		public NextAction(String name, Icon icon){
+			super(name, icon);
+		}
+		public void actionPerformed(ActionEvent ae){
+			setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+			System.out.println("Next!");
+			setCursor(Cursor.getDefaultCursor());
+		}
+	}
+
+	/**
+	* Action that goes to the first timestep
+	*/
+	private class FirstAction extends AbstractAction{
+		public FirstAction(String name, Icon icon){
+			super(name, icon);
+		}
+		public void actionPerformed(ActionEvent ae){
+			setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+			System.out.println("First!");
+			setCursor(Cursor.getDefaultCursor());
+		}
+	}
 }
 
 
