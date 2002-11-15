c1516108c3dfc406145726580869cb192a472a8a
scott <scottbell@users.noreply.github.com>
2002-11-15 20:21:53 +0000

    none

--- BIOSIM/src/com/traclabs/biosim/server/simulation/water/DirtyWaterStoreServer.java
+++ BIOSIM/src/com/traclabs/biosim/server/simulation/water/DirtyWaterStoreServer.java
@@ -1,46 +1,23 @@
 package biosim.server.water;
 
+import biosim.server.framework.*;
 import biosim.idl.water.*;
-import biosim.server.util.*;
-import org.omg.CosNaming.*;
-import org.omg.CosNaming.NamingContextPackage.*;
-import org.omg.CORBA.*;
-import org.omg.PortableServer.*;
-import org.omg.PortableServer.POA;
 /**
- * The Dirty Water Store Server.  Creates an instance of the DirtyWaterStore and registers it with the nameserver.
+ * The Dirty Water Store Server.  Creates an instance of the Dirty Water Store and registers it with the nameserver.
  *
  * @author    Scott Bell
  */
 
-public class DirtyWaterStoreServer {
+public class DirtyWaterStoreServer extends GenericServer{
+	
 	/**
 	* Instantiates the server and binds it to the name server.
 	* @param args aren't used for anything
 	*/
 	public static void main(String args[]) {
-		try{
-			// create and initialize the ORB
-			ORB orb = OrbUtils.getORB();
-			NamingContextExt ncRef = OrbUtils.getNCRef();
-			POA rootpoa = OrbUtils.getRootPOA();
-			rootpoa.the_POAManager().activate();
-			// create servant and register it with  ORB
-			DirtyWaterStoreImpl myDirtyWaterStoreImpl = new DirtyWaterStoreImpl(0);
-			// get object reference from the servant
-			org.omg.CORBA.Object ref =rootpoa.servant_to_reference(new DirtyWaterStorePOATie(myDirtyWaterStoreImpl));
-			// bind the Object Reference in Naming
-			NameComponent path[] = ncRef.to_name("DirtyWaterStore");
-			ncRef.rebind(path, ref);
-			System.out.println("DirtyWaterStore Server ready and waiting ...");
-			// wait for invocations from clients
-			orb.run();
-		}
-		catch (Exception e) {
-			System.err.println("ERROR: " + e);
-			e.printStackTrace();
-		}
-		System.out.println("DirtyWaterStore Server Exiting ...");
+		DirtyWaterStoreServer myServer = new DirtyWaterStoreServer();
+		DirtyWaterStoreImpl myDirtyWaterStore = new DirtyWaterStoreImpl(0);
+		myServer.registerServerAndRun(new DirtyWaterStorePOATie(myDirtyWaterStore), myDirtyWaterStore.getModuleName());
 	}
 }
 
--- BIOSIM/src/com/traclabs/biosim/server/simulation/water/PotableWaterStoreServer.java
+++ BIOSIM/src/com/traclabs/biosim/server/simulation/water/PotableWaterStoreServer.java
@@ -1,46 +1,23 @@
 package biosim.server.water;
 
+import biosim.server.framework.*;
 import biosim.idl.water.*;
-import biosim.server.util.*;
-import org.omg.CosNaming.*;
-import org.omg.CosNaming.NamingContextPackage.*;
-import org.omg.CORBA.*;
-import org.omg.PortableServer.*;
-import org.omg.PortableServer.POA;
 /**
- * The Potable Water Store Server.  Creates an instance of the PotableWaterStore and registers it with the nameserver.
+ * The Potable Water Store Server.  Creates an instance of the Potable Water Store and registers it with the nameserver.
  *
  * @author    Scott Bell
  */
 
-public class PotableWaterStoreServer {
+public class PotableWaterStoreServer extends GenericServer{
+	
 	/**
 	* Instantiates the server and binds it to the name server.
 	* @param args aren't used for anything
 	*/
 	public static void main(String args[]) {
-		try{
-			// create and initialize the ORB
-			ORB orb = OrbUtils.getORB();
-			NamingContextExt ncRef = OrbUtils.getNCRef();
-			POA rootpoa = OrbUtils.getRootPOA();
-			rootpoa.the_POAManager().activate();
-			// create servant and register it with  ORB
-			PotableWaterStoreImpl myPotableWaterStoreImpl = new PotableWaterStoreImpl(0);
-			// get object reference from the servant
-			org.omg.CORBA.Object ref =rootpoa.servant_to_reference(new PotableWaterStorePOATie(myPotableWaterStoreImpl));
-			// bind the Object Reference in Naming
-			NameComponent path[] = ncRef.to_name("PotableWaterStore");
-			ncRef.rebind(path, ref);
-			System.out.println("PotableWaterStore Server ready and waiting ...");
-			// wait for invocations from clients
-			orb.run();
-		}
-		catch (Exception e) {
-			System.err.println("ERROR: " + e);
-			e.printStackTrace();
-		}
-		System.out.println("PotableWaterStore Server Exiting ...");
+		PotableWaterStoreServer myServer = new PotableWaterStoreServer();
+		PotableWaterStoreImpl myPotableWaterStoreImpl = new PotableWaterStoreImpl(0);
+		myServer.registerServerAndRun(new PotableWaterStorePOATie(myPotableWaterStoreImpl), myPotableWaterStoreImpl.getModuleName());
 	}
 }
 
