8e8129ad2abe9386c29899c0a34196be2c031b4f
scott <scottbell@users.noreply.github.com>
2002-11-15 19:50:47 +0000

    none

--- BIOSIM/src/com/traclabs/biosim/server/simulation/air/AirRSServer.java
+++ BIOSIM/src/com/traclabs/biosim/server/simulation/air/AirRSServer.java
@@ -1,47 +1,23 @@
 package biosim.server.air;
 
+import biosim.server.framework.*;
 import biosim.idl.air.*;
-import org.omg.CosNaming.*;
-import org.omg.CosNaming.NamingContextPackage.*;
-import org.omg.CORBA.*;
-import org.omg.PortableServer.*;
-import org.omg.PortableServer.POA;
-import biosim.server.util.*;
 /**
  * The Air Revitalization System Server.  Creates an instance of the AirRS and registers it with the nameserver.
  *
  * @author    Scott Bell
  */
 
-public class AirRSServer {
+public class AirRSServer extends GenericServer{
 	
 	/**
 	* Instantiates the server and binds it to the name server.
 	* @param args aren't used for anything
 	*/
 	public static void main(String args[]) {
-		try{
-			// create and initialize the ORB
-			ORB orb = OrbUtils.getORB();
-			NamingContextExt ncRef = OrbUtils.getNCRef();
-			POA rootpoa = OrbUtils.getRootPOA();
-			rootpoa.the_POAManager().activate();
-			// create servant and register it with  ORB
-			AirRSImpl myAirRSImpl = new AirRSImpl(0);
-			// get object reference from the servant
-			org.omg.CORBA.Object ref =rootpoa.servant_to_reference(new AirRSPOATie(myAirRSImpl));
-			// bind the Object Reference in Naming
-			NameComponent path[] = ncRef.to_name("AirRS");
-			ncRef.rebind(path, ref);
-			System.out.println("AirRS Server ready and waiting ...");
-			// wait for invocations from clients
-			orb.run();
-		}
-		catch (Exception e) {
-			System.err.println("ERROR: " + e);
-			e.printStackTrace(System.out);
-		}
-		System.out.println("AirRS Server Exiting ...");
+		AirRSServer myServer = new AirRSServer();
+		AirRSImpl myAirRS = new AirRSImpl(0);
+		myServer.registerServerAndRun(new AirRSPOATie(myAirRS), myAirRS.getModuleName());
 	}
 }
 
--- BIOSIM/src/com/traclabs/biosim/server/simulation/crew/CrewGroupServer.java
+++ BIOSIM/src/com/traclabs/biosim/server/simulation/crew/CrewGroupServer.java
@@ -1,47 +1,23 @@
 package biosim.server.crew;
 
+import biosim.server.framework.*;
 import biosim.idl.crew.*;
-import biosim.server.util.*;
-import org.omg.CosNaming.*;
-import org.omg.CosNaming.NamingContextPackage.*;
-import org.omg.CORBA.*;
-import org.omg.PortableServer.*;
-import org.omg.PortableServer.POA;
 /**
- * The Crew Server.  Creates an instance of the CrewGroup and registers it with the nameserver.
+ * The Air Revitalization System Server.  Creates an instance of the AirRS and registers it with the nameserver.
  *
  * @author    Scott Bell
  */
 
-public class CrewGroupServer {
+public class CrewGroupServer extends GenericServer{
 	
 	/**
 	* Instantiates the server and binds it to the name server.
 	* @param args aren't used for anything
 	*/
 	public static void main(String args[]) {
-		try{
-			// create and initialize the ORB
-			ORB orb = OrbUtils.getORB();
-			NamingContextExt ncRef = OrbUtils.getNCRef();
-			POA rootpoa = OrbUtils.getRootPOA();
-			rootpoa.the_POAManager().activate();
-			// create servant and register it with  ORB
-			CrewGroupImpl myCrewImpl = new CrewGroupImpl(0);
-			// get object reference from the servant
-			org.omg.CORBA.Object ref =rootpoa.servant_to_reference(new CrewGroupPOATie(myCrewImpl));
-			// bind the Object Reference in Naming
-			NameComponent path[] = ncRef.to_name("CrewGroup");
-			ncRef.rebind(path, ref);
-			System.out.println("CrewGroup Server ready and waiting ...");
-			// wait for invocations from clients
-			orb.run();
-		}
-		catch (Exception e) {
-			System.err.println("ERROR: " + e);
-			e.printStackTrace(System.out);
-		}
-		System.out.println("Crew Server Exiting ...");
+		CrewGroupServer myServer = new CrewGroupServer();
+		CrewGroupImpl myCrewGroup = new CrewGroupImpl(0);
+		myServer.registerServerAndRun(new CrewGroupPOATie(myCrewGroup), myCrewGroup.getModuleName());
 	}
 }
 
