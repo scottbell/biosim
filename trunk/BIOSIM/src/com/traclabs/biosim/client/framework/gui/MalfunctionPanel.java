adb5e37daa5154ef12a2d7d6bf4269394b70ece2
scott <scott@4576cf88-1eed-0310-b270-a9f98363dfe6>
2002-12-10 17:49:36 +0000

    none

---
+++ trunk/BIOSIM/src/com/traclabs/biosim/client/framework/gui/MalfunctionPanel.java
@@ -0,0 +1,27 @@
+package biosim.client.framework.gui;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/**
+ * 
+ * @author    Scott Bell
+ */
+public class MalfunctionPanel extends TimedPanel
+{
+
+	/**
+	 * Default constructor.
+	 */
+	public GraphPanel() {
+		super();
+		buildGui();
+	}
+	
+	public void refresh(){
+	}
+
+	protected void buildGui(){
+	}
+}
