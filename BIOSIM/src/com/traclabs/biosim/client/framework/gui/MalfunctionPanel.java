8939e296d6eb71b3be8ce2e322ad3255cd9ca996
scott <scottbell@users.noreply.github.com>
2002-12-13 23:56:13 +0000

    none

--- BIOSIM/src/com/traclabs/biosim/client/framework/gui/MalfunctionPanel.java
+++ BIOSIM/src/com/traclabs/biosim/client/framework/gui/MalfunctionPanel.java
@@ -3,6 +3,7 @@ package biosim.client.framework.gui;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.table.*;
 
 /**
  * 
@@ -10,7 +11,7 @@ import javax.swing.*;
  */
 public class MalfunctionPanel extends TimedPanel
 {
-	
+	JTable myTable;
 	/**
 	 * Default constructor.
 	 */
@@ -23,5 +24,22 @@ public class MalfunctionPanel extends TimedPanel
 	}
 
 	protected void buildGui(){
+		setLayout(new BorderLayout());
+		DefaultTableModel myTableModel = new DefaultTableModel();
+		myTableModel.addColumn("Module");
+		myTableModel.addColumn("Status");
+		myTableModel.addColumn("Malfunction");
+		myTable = new JTable(myTableModel);
+		add(myTable, BorderLayout.CENTER);
+		String[] rowValues = {"one", "two", "three"};
+		myTableModel.addRow(rowValues);
+	}
+	
+	public static void main(String[] args){
+		BioFrame myFrame = new BioFrame("Malfunctions", false);
+		MalfunctionPanel myMalfPanel = new MalfunctionPanel();
+		myFrame.getContentPane().add(myMalfPanel);
+		myFrame.pack();
+		myFrame.setVisible(true);
 	}
 }
