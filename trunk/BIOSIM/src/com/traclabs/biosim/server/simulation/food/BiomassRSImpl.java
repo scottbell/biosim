357d0ff03057c458079b48f23ffa3ff3e7efe846
scott <scott@4576cf88-1eed-0310-b270-a9f98363dfe6>
2003-01-28 18:14:05 +0000

    none

--- trunk/BIOSIM/src/com/traclabs/biosim/server/simulation/food/BiomassRSImpl.java
+++ trunk/BIOSIM/src/com/traclabs/biosim/server/simulation/food/BiomassRSImpl.java
@@ -15,7 +15,7 @@ import java.util.*;
 
 public class BiomassRSImpl extends BioModuleImpl implements BiomassRSOperations {
 	private Vector myShelves;
-	private int shelfCapacity = 1000;
+	private int shelfCapacity = 100;
 	private Vector shelfLogs;
 	
 	public BiomassRSImpl(int pID){
