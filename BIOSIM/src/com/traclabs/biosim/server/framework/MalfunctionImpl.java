4721efa71db15431090c3def424a6600e50cfaba
scott <scottbell@users.noreply.github.com>
2003-02-02 05:35:20 +0000

    none

--- BIOSIM/src/com/traclabs/biosim/server/framework/MalfunctionImpl.java
+++ BIOSIM/src/com/traclabs/biosim/server/framework/MalfunctionImpl.java
@@ -7,25 +7,43 @@ import biosim.idl.framework.*;
  */
 
 public class MalfunctionImpl extends MalfunctionPOA  {
-	private static int lastID = 0;
+	private static long lastID = 0;
 	private int myID;
 	private MalfunctionIntensity myIntensity;
 	private MalfunctionLength myLength;
 	private String myName;
 	private boolean performed;
+	private int repairTime = 0;
+	private int maxRepairTime = 1;
 	
 	public MalfunctionImpl(String pName, MalfunctionIntensity pIntensity, MalfunctionLength pLength){
-		if (lastID > java.lang.Integer.MAX_VALUE)
+		if (lastID > java.lang.Long.MAX_VALUE)
 			lastID = 0;
 		lastID++;
 		myID = lastID;
 		myName = pName;
 		myIntensity = pIntensity;
 		myLength = pLength;
+		if (myLength == MalfunctionLength.TEMPORARY_MALF)
+			if (myIntensity == MalfunctionIntensity.LOW_MALF)
+				maxRepairTime = 1;
+			else if (myIntensity == MalfunctionIntensity.MEDIUM_MALF)
+				maxRepairTime = 2;
+			if (myIntensity == MalfunctionIntensity.SEVERE_MALF)
+				maxRepairTime = 4;	
 	}
 	
 	public int getID(){
 		return myID;
+	}
+	
+	public void repair(){
+		if (myLength == MalfunctionLength.TEMPORARY_MALF)
+			repairTime++;
+	}
+	
+	public boolean isRepaired(){
+		return (repairTime >= maxRepairTime);
 	}
 	
 	public boolean hasPerformed(){
