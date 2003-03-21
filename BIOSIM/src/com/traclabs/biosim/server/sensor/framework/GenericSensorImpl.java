14311b9bf076ea6c6ad8f784dbe577513e240ff1
scott <scottbell@users.noreply.github.com>
2003-03-21 17:43:37 +0000

    none

--- BIOSIM/src/com/traclabs/biosim/server/sensor/framework/GenericSensorImpl.java
+++ BIOSIM/src/com/traclabs/biosim/server/sensor/framework/GenericSensorImpl.java
@@ -2,22 +2,15 @@ package biosim.server.sensor.framework;
 
 import biosim.server.framework.*;
 import biosim.idl.sensor.framework.*;
-import java.util.*;
 
 public abstract class GenericSensor extends BioModuleImpl implements GenericSensorOperations{
-	private List listeners;
 	
 	public GenericSensor(int pID){
 		super(pID);
-		listeners = new Vector();
 	}
 	
 	protected abstract void gatherData();
 	protected abstract void processData();
 	protected abstract void notifyListeners();
-	
 	public abstract double getValue();
-	
-	public void registerListener(){
-	}
 }
