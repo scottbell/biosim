7a3d7915dbb806a9e298d1fd953bbec6b34eff6f
scott <scott@4576cf88-1eed-0310-b270-a9f98363dfe6>
2003-04-11 19:15:36 +0000

    none

--- trunk/BIOSIM/src/com/traclabs/biosim/client/util/BioHolder.java
+++ trunk/BIOSIM/src/com/traclabs/biosim/client/util/BioHolder.java
@@ -7,6 +7,11 @@ import biosim.idl.simulation.food.*;
 import biosim.idl.simulation.power.*;
 import biosim.idl.simulation.water.*;
 import biosim.idl.simulation.framework.*;
+import biosim.idl.sensor.air.*;
+import biosim.idl.sensor.environment.*;
+import biosim.idl.sensor.food.*;
+import biosim.idl.sensor.power.*;
+import biosim.idl.sensor.water.*;
 import biosim.idl.framework.*;
 import java.util.*;
 import biosim.client.util.*;
@@ -41,64 +46,64 @@ public class BioHolder
 	//Sensor
 	//Air
 	//AirRs
-	private String myAirRSPowerInFlowRateSensorName = "AirRSPowerInFlowRateSensor";
-	private String myAirRSAirInFlowRateSensorName = "AirRSAirInFlowRateSensor";
-	private String myAirRSAirOutFlowRateSensorName = "AirRSAirOutFlowRateSensor";
-	private String myAirRSO2OutFlowRateSensorName = "AirRSO2OutFlowRateSensor";
-	private String myAirRSCO2InFlowRateSensorName = "AirRSCO2InFlowRateSensor";
-	private String myAirRSCO2OutFlowRateSensorName = "AirRSCO2OutFlowRateSensor";
+	public final static String myAirRSPowerInFlowRateSensorName = "AirRSPowerInFlowRateSensor";
+	public final static String myAirRSAirInFlowRateSensorName = "AirRSAirInFlowRateSensor";
+	public final static String myAirRSAirOutFlowRateSensorName = "AirRSAirOutFlowRateSensor";
+	public final static String myAirRSO2OutFlowRateSensorName = "AirRSO2OutFlowRateSensor";
+	public final static String myAirRSCO2InFlowRateSensorName = "AirRSCO2InFlowRateSensor";
+	public final static String myAirRSCO2OutFlowRateSensorName = "AirRSCO2OutFlowRateSensor";
 	//Stores
-	private String myO2StoreLevelSensorName = "O2StoreLevelSensor";
-	private String myCO2StoreLevelSensorName = "CO2StoreLevel";
+	public final static String myO2StoreLevelSensorName = "O2StoreLevelSensor";
+	public final static String myCO2StoreLevelSensorName = "CO2StoreLevel";
 	//Power
 	//PowerPS
-	private String myPowerPSPowerOutFlowRateSensorName = "PowerPSPowerOutFlowRateSensor";
+	public final static String myPowerPSPowerOutFlowRateSensorName = "PowerPSPowerOutFlowRateSensor";
 	//Stores
-	private String myPowerStoreLevelSensorName = "PowerStoreLevelSensor";
+	public final static String myPowerStoreLevelSensorName = "PowerStoreLevelSensor";
 	//Environment
 	//Crew
-	private String myCrewEnvironmentOtherAirLevelSensorName = "CrewEnvironmentOtherAirLevelSensor";
-	private String myCrewEnvironmentCO2AirLevelSensorName = "CrewEnvironmentCO2AirLevelSensor";
-	private String myCrewEnvironmentO2AirLevelSensorName = "CrewEnvironmentO2AirLevelSensor";
+	public final static String myCrewEnvironmentOtherAirLevelSensorName = "CrewEnvironmentOtherAirLevelSensor";
+	public final static String myCrewEnvironmentCO2AirLevelSensorName = "CrewEnvironmentCO2AirLevelSensor";
+	public final static String myCrewEnvironmentO2AirLevelSensorName = "CrewEnvironmentO2AirLevelSensor";
 	//Plant
-	private String myPlantEnvironmentOtherAirLevelSensorName = "PlantEnvironmentOtherAirLevelSensor";
-	private String myPlantEnvironmentCO2AirLevelSensorName = "PlantEnvironmentCO2AirLevelSensor";
-	private String myPlantEnvironmentO2AirLevelSensorName = "PlantEnvironmentO2AirLevelSensor";
+	public final static String myPlantEnvironmentOtherAirLevelSensorName = "PlantEnvironmentOtherAirLevelSensor";
+	public final static String myPlantEnvironmentCO2AirLevelSensorName = "PlantEnvironmentCO2AirLevelSensor";
+	public final static String myPlantEnvironmentO2AirLevelSensorName = "PlantEnvironmentO2AirLevelSensor";
 	//Water
 	//WaterRS
-	private String myWaterRSDirtyWaterInFlowRateSensorName = "WaterRSDirtyWaterInFlowRateSensor";
-	private String myWaterRSGreyWaterInFlowRateSensorName = "WaterRSGreyWaterInFlowRateSensor";
-	private String myWaterRSPowerInFlowRateSensorName = "WaterRSPowerInFlowRateSensor";
-	private String myWaterRSPotableWaterOutFlowRateSensorName = "WaterRSPotableWaterOutFlowRateSensor";
+	public final static String myWaterRSDirtyWaterInFlowRateSensorName = "WaterRSDirtyWaterInFlowRateSensor";
+	public final static String myWaterRSGreyWaterInFlowRateSensorName = "WaterRSGreyWaterInFlowRateSensor";
+	public final static String myWaterRSPowerInFlowRateSensorName = "WaterRSPowerInFlowRateSensor";
+	public final static String myWaterRSPotableWaterOutFlowRateSensorName = "WaterRSPotableWaterOutFlowRateSensor";
 	//Stores
-	private String myPotableWaterStoreLevelSensorName = "PotableWaterStoreLevelSensor";
-	private String myGreyWaterStoreLevelSensorName = "GreyWaterStoreLevelSensor";
-	private String myDirtyWaterStoreLevelSensorName = "DirtyWaterStoreLevelSensor";
+	public final static String myPotableWaterStoreLevelSensorName = "PotableWaterStoreLevelSensor";
+	public final static String myGreyWaterStoreLevelSensorName = "GreyWaterStoreLevelSensor";
+	public final static String myDirtyWaterStoreLevelSensorName = "DirtyWaterStoreLevelSensor";
 	//Food
 	//BiomassRS
-	private String myBiomassRSAirInFlowRateSensorName = "BiomassRSAirInFlowRateSensor";
-	private String myBiomassRSAirOutFlowRateSensorName = "BiomassRSAirOutFlowRateSensor";
-	private String myBiomassRSPotableWaterInFlowRateSensorName = "BiomassRSPotableWaterInFlowRateSensor";
-	private String myBiomassRSGreyWaterInFlowRateSensorName = "BiomassRSGreyWaterInFlowRateSensor";
-	private String myBiomassRSBiomassOutFlowRateSensorName = "BiomassRSBiomassOutFlowRateSensor";
+	public final static String myBiomassRSAirInFlowRateSensorName = "BiomassRSAirInFlowRateSensor";
+	public final static String myBiomassRSAirOutFlowRateSensorName = "BiomassRSAirOutFlowRateSensor";
+	public final static String myBiomassRSPotableWaterInFlowRateSensorName = "BiomassRSPotableWaterInFlowRateSensor";
+	public final static String myBiomassRSGreyWaterInFlowRateSensorName = "BiomassRSGreyWaterInFlowRateSensor";
+	public final static String myBiomassRSBiomassOutFlowRateSensorName = "BiomassRSBiomassOutFlowRateSensor";
 	//Food Processor
-	private String myFoodProcessorPowerInFlowRateSensorName = "FoodProcessorPowerInFlowRateSensor";
-	private String myFoodProcessorBiomassInFlowRateSensorName = "FoodProcessorBiomassInFlowRateSensor";
-	private String myFoodProcessorFoodOutFlowRateSensorName = "FoodProcessorFoodOutFlowRateSensor";
+	public final static String myFoodProcessorPowerInFlowRateSensorName = "FoodProcessorPowerInFlowRateSensor";
+	public final static String myFoodProcessorBiomassInFlowRateSensorName = "FoodProcessorBiomassInFlowRateSensor";
+	public final static String myFoodProcessorFoodOutFlowRateSensorName = "FoodProcessorFoodOutFlowRateSensor";
 	//Stores
-	private String myBiomassStoreLevelSensorName = "BiomassStoreLevelSensor";
-	private String myFoodStoreLevelSensorName = "FoodStoreLevelSensor";
+	public final static String myBiomassStoreLevelSensorName = "BiomassStoreLevelSensor";
+	public final static String myFoodStoreLevelSensorName = "FoodStoreLevelSensor";
 	//Framework
 	//Accumulator
-	private String myAccumulatorCO2AirEnvironmentInFlowRateSensorName = "AccumulatorCO2AirEnvironmentInFlowRateSensor";
-	private String myAccumulatorO2AirEnvironmentInFlowRateSensorName = "AccumulatorO2AirEnvironmentInFlowRateSensor";
-	private String myAccumulatorCO2AirStoreOutFlowRateSensorName = "AccumulatorCO2AirStoreOutFlowRateSensor";
-	private String myAccumulatorO2AirStoreOutFlowRateSensorName = "AccumulatorO2AirStoreOutFlowRateSensor";
+	public final static String myAccumulatorCO2AirEnvironmentInFlowRateSensorName = "AccumulatorCO2AirEnvironmentInFlowRateSensor";
+	public final static String myAccumulatorO2AirEnvironmentInFlowRateSensorName = "AccumulatorO2AirEnvironmentInFlowRateSensor";
+	public final static String myAccumulatorCO2AirStoreOutFlowRateSensorName = "AccumulatorCO2AirStoreOutFlowRateSensor";
+	public final static String myAccumulatorO2AirStoreOutFlowRateSensorName = "AccumulatorO2AirStoreOutFlowRateSensor";
 	//Injector
-	private String myInjectorCO2AirStoreInFlowRateSensorName = "InjectorCO2AirStoreInFlowRateSensor";
-	private String myInjectorO2AirStoreInFlowRateSensorName = "InjectorO2AirStoreInFlowRateSensor";
-	private String myInjectorCO2AirEnvironmentOutFlowRateSensorName = "InjectorCO2AirEnvironmentOutFlowRateSensor";
-	private String myInjectorO2AirEnvironmentOutFlowRateSensorName = "InjectorO2AirEnvironmentOutFlowRateSensor";
+	public final static String myInjectorCO2AirStoreInFlowRateSensorName = "InjectorCO2AirStoreInFlowRateSensor";
+	public final static String myInjectorO2AirStoreInFlowRateSensorName = "InjectorO2AirStoreInFlowRateSensor";
+	public final static String myInjectorCO2AirEnvironmentOutFlowRateSensorName = "InjectorCO2AirEnvironmentOutFlowRateSensor";
+	public final static String myInjectorO2AirEnvironmentOutFlowRateSensorName = "InjectorO2AirEnvironmentOutFlowRateSensor";
 
 	//A hastable containing the server references
 	private static Map modules;
@@ -123,13 +128,13 @@ public class BioHolder
 		}
 		return returnModule;
 	}
-	
+
 	public static BioModule[] getBioModules(){
 		collectReferences();
 		BioModule[] arrayModules = new BioModule[modules.size()];
 		return (BioModule[])(modules.values().toArray(arrayModules));
 	}
-	
+
 	public static String[] getBioModuleNames(){
 		collectReferences();
 		String[] arrayModuleNames = new String[modules.size()];
@@ -157,7 +162,7 @@ public class BioHolder
 		try{
 			if (modules == null)
 				modules = new Hashtable();
-			System.err.println("BioHolder: Collecting references to modules...");
+			System.err.println("BioHolder: Collecting simulation references to modules...");
 			CrewGroup myCrew = CrewGroupHelper.narrow(OrbUtils.getNCRef().resolve_str(crewName+myID));
 			modules.put(crewName , myCrew);
 			PowerPS myPowerPS = PowerPSHelper.narrow(OrbUtils.getNCRef().resolve_str(powerPSName+myID));
@@ -190,6 +195,85 @@ public class BioHolder
 			modules.put(biomassStoreName, myBiomassStore);
 			WaterRS myWaterRS = WaterRSHelper.narrow(OrbUtils.getNCRef().resolve_str(waterRSName+myID));
 			modules.put(waterRSName , myWaterRS);
+
+
+			System.err.println("BioHolder: Collecting sensor references to modules...");
+			//Air
+			{
+				//AirRS
+				{
+					PowerInFlowRateSensor myAirRSPowerInFlowRateSensor = PowerInFlowRateSensorHelper.narrow(OrbUtils.getNCRef().resolve_str(myAirRSPowerInFlowRateSensorName+myID));
+					modules.put(myAirRSPowerInFlowRateSensorName , myAirRSPowerInFlowRateSensor);
+					AirInFlowRateSensor myAirRSAirInFlowRateSensor = AirInFlowRateSensorHelper.narrow(OrbUtils.getNCRef().resolve_str(myAirRSAirInFlowRateSensorName+myID));
+					modules.put(myAirRSAirInFlowRateSensorName , myAirRSAirInFlowRateSensor);
+					AirOutFlowRateSensor myAirRSAirOutFlowRateSensor = AirOutFlowRateSensorHelper.narrow(OrbUtils.getNCRef().resolve_str(myAirRSAirOutFlowRateSensorName+myID));
+					modules.put(myAirRSAirOutFlowRateSensorName , myAirRSAirOutFlowRateSensor);
+					O2OutFlowRateSensor myAirRSO2OutFlowRateSensor = O2OutFlowRateSensorHelper.narrow(OrbUtils.getNCRef().resolve_str(myAirRSO2OutFlowRateSensorName+myID));
+					modules.put(myAirRSO2OutFlowRateSensorName , myAirRSO2OutFlowRateSensor);
+					CO2InFlowRateSensor myAirRSCO2InFlowRateSensor = CO2InFlowRateSensorHelper.narrow(OrbUtils.getNCRef().resolve_str(myAirRSCO2InFlowRateSensorName+myID));
+					modules.put(myAirRSCO2InFlowRateSensorName , myAirRSCO2InFlowRateSensor);
+					CO2OutFlowRateSensor myAirRSCO2OutFlowRateSensor = CO2OutFlowRateSensorHelper.narrow(OrbUtils.getNCRef().resolve_str(myAirRSCO2OutFlowRateSensorName+myID));
+					modules.put(myAirRSCO2OutFlowRateSensorName , myAirRSCO2OutFlowRateSensor);
+				}
+			}
+
+
+			/*
+			//Stores
+			myO2StoreLevelSensorName = "O2StoreLevelSensor";
+			myCO2StoreLevelSensorName = "CO2StoreLevel";
+			//Power
+			//PowerPS
+			myPowerPSPowerOutFlowRateSensorName = "PowerPSPowerOutFlowRateSensor";
+			//Stores
+			myPowerStoreLevelSensorName = "PowerStoreLevelSensor";
+			//Environment
+			//Crew
+			myCrewEnvironmentOtherAirLevelSensorName = "CrewEnvironmentOtherAirLevelSensor";
+			myCrewEnvironmentCO2AirLevelSensorName = "CrewEnvironmentCO2AirLevelSensor";
+			myCrewEnvironmentO2AirLevelSensorName = "CrewEnvironmentO2AirLevelSensor";
+			//Plant
+			myPlantEnvironmentOtherAirLevelSensorName = "PlantEnvironmentOtherAirLevelSensor";
+			myPlantEnvironmentCO2AirLevelSensorName = "PlantEnvironmentCO2AirLevelSensor";
+			myPlantEnvironmentO2AirLevelSensorName = "PlantEnvironmentO2AirLevelSensor";
+			//Water
+			//WaterRS
+			myWaterRSDirtyWaterInFlowRateSensorName = "WaterRSDirtyWaterInFlowRateSensor";
+			myWaterRSGreyWaterInFlowRateSensorName = "WaterRSGreyWaterInFlowRateSensor";
+			myWaterRSPowerInFlowRateSensorName = "WaterRSPowerInFlowRateSensor";
+			myWaterRSPotableWaterOutFlowRateSensorName = "WaterRSPotableWaterOutFlowRateSensor";
+			//Stores
+			myPotableWaterStoreLevelSensorName = "PotableWaterStoreLevelSensor";
+			myGreyWaterStoreLevelSensorName = "GreyWaterStoreLevelSensor";
+			myDirtyWaterStoreLevelSensorName = "DirtyWaterStoreLevelSensor";
+			//Food
+			//BiomassRS
+			myBiomassRSAirInFlowRateSensorName = "BiomassRSAirInFlowRateSensor";
+			myBiomassRSAirOutFlowRateSensorName = "BiomassRSAirOutFlowRateSensor";
+			myBiomassRSPotableWaterInFlowRateSensorName = "BiomassRSPotableWaterInFlowRateSensor";
+			myBiomassRSGreyWaterInFlowRateSensorName = "BiomassRSGreyWaterInFlowRateSensor";
+			myBiomassRSBiomassOutFlowRateSensorName = "BiomassRSBiomassOutFlowRateSensor";
+			//Food Processor
+			myFoodProcessorPowerInFlowRateSensorName = "FoodProcessorPowerInFlowRateSensor";
+			myFoodProcessorBiomassInFlowRateSensorName = "FoodProcessorBiomassInFlowRateSensor";
+			myFoodProcessorFoodOutFlowRateSensorName = "FoodProcessorFoodOutFlowRateSensor";
+			//Stores
+			myBiomassStoreLevelSensorName = "BiomassStoreLevelSensor";
+			myFoodStoreLevelSensorName = "FoodStoreLevelSensor";
+			//Framework
+			//Accumulator
+			myAccumulatorCO2AirEnvironmentInFlowRateSensorName = "AccumulatorCO2AirEnvironmentInFlowRateSensor";
+			myAccumulatorO2AirEnvironmentInFlowRateSensorName = "AccumulatorO2AirEnvironmentInFlowRateSensor";
+			myAccumulatorCO2AirStoreOutFlowRateSensorName = "AccumulatorCO2AirStoreOutFlowRateSensor";
+			myAccumulatorO2AirStoreOutFlowRateSensorName = "AccumulatorO2AirStoreOutFlowRateSensor";
+			//Injector
+			myInjectorCO2AirStoreInFlowRateSensorName = "InjectorCO2AirStoreInFlowRateSensor";
+			myInjectorO2AirStoreInFlowRateSensorName = "InjectorO2AirStoreInFlowRateSensor";
+			myInjectorCO2AirEnvironmentOutFlowRateSensorName = "InjectorCO2AirEnvironmentOutFlowRateSensor";
+			myInjectorO2AirEnvironmentOutFlowRateSensorName = "InjectorO2AirEnvironmentOutFlowRateSensor";
+			*/
+
+			System.err.println("BioHolder: Collecting framework references to modules...");
 			myBioDriver = BioDriverHelper.narrow(OrbUtils.getNCRef().resolve_str("BioDriver"+myID));
 			hasCollectedReferences = true;
 		}
