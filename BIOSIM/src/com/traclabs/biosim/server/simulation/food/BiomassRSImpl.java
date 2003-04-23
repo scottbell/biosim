ebf1f438a80b811e52d147fc1671e56b7b579d3a
scott <scottbell@users.noreply.github.com>
2003-04-23 19:54:22 +0000

    none

--- BIOSIM/src/com/traclabs/biosim/server/simulation/food/BiomassRSImpl.java
+++ BIOSIM/src/com/traclabs/biosim/server/simulation/food/BiomassRSImpl.java
@@ -55,6 +55,7 @@ public class BiomassRSImpl extends SimBioModuleImpl implements BiomassRSOperatio
 
 	public BiomassRSImpl(int pID){
 		super(pID);
+		myShelves = new Vector();
 		myGreyWaterStores = new GreyWaterStore[0];
 		myPotableWaterStores = new PotableWaterStore[0];
 		myPowerStores = new PowerStore[0];
@@ -79,93 +80,6 @@ public class BiomassRSImpl extends SimBioModuleImpl implements BiomassRSOperatio
 		greyWaterDesiredFlowRates = new float[0];
 		airInDesiredFlowRates = new float[0];
 		airOutDesiredFlowRates = new float[0];
-		myShelves = new Vector(shelfCapacity);
-		for (int i = 0; i < shelfCapacity; i++){
-			myShelves.add(new ShelfImpl(pID, this));
-		}
-	}
-
-	public BiomassRSImpl(int pID, int pShelfCapacity){
-		super(pID);
-		shelfCapacity = pShelfCapacity;
-		myShelves = new Vector(shelfCapacity);
-		for (int i = 0; i < shelfCapacity; i++){
-			myShelves.add(new ShelfImpl(pID, this));
-		}
-	}
-
-	public float getTotalPotableWaterConsumed(){
-		float theWater = 0.0f;
-		for (Iterator iter = myShelves.iterator(); iter.hasNext();){
-			ShelfImpl currentShelf = (ShelfImpl)(iter.next());
-			theWater += currentShelf.getPotableWaterConsumed();
-		}
-		return theWater;
-	}
-
-	public float getTotalBiomassProduced(){
-		float theBiomass = 0.0f;
-		for (Iterator iter = myShelves.iterator(); iter.hasNext();){
-			ShelfImpl currentShelf = (ShelfImpl)(iter.next());
-			theBiomass += currentShelf.getBiomassProduced();
-		}
-		return theBiomass;
-	}
-
-	public float getTotalCO2Consumed(){
-		float theCO2Consumed = 0.0f;
-		for (Iterator iter = myShelves.iterator(); iter.hasNext();){
-			ShelfImpl currentShelf = (ShelfImpl)(iter.next());
-			theCO2Consumed += currentShelf.getCO2Consumed();
-		}
-		return theCO2Consumed;
-	}
-
-	public float getTotalO2Produced(){
-		float theO2Produced = 0.0f;
-		for (Iterator iter = myShelves.iterator(); iter.hasNext();){
-			ShelfImpl currentShelf = (ShelfImpl)(iter.next());
-			theO2Produced += currentShelf.getO2Produced();
-		}
-		return theO2Produced;
-	}
-
-	public float getTotalGreyWaterConsumed(){
-		float theWater = 0.0f;
-		for (Iterator iter = myShelves.iterator(); iter.hasNext();){
-			ShelfImpl currentShelf = (ShelfImpl)(iter.next());
-			theWater += currentShelf.getGreyWaterConsumed();
-		}
-		return theWater;
-	}
-
-	public float getTotalPowerConsumed(){
-		float thePower = 0.0f;
-		for (Iterator iter = myShelves.iterator(); iter.hasNext();){
-			ShelfImpl currentShelf = (ShelfImpl)(iter.next());
-			thePower += currentShelf.getPowerConsumed();
-		}
-		return thePower;
-	}
-
-	public float getTotalPlantArea(){
-		float thePlantArea = 0.0f;
-		for (Iterator iter = myShelves.iterator(); iter.hasNext();){
-			ShelfImpl currentShelf = (ShelfImpl)(iter.next());
-			thePlantArea += currentShelf.getArea();
-		}
-		return thePlantArea;
-	}
-
-	public String[] getPlantTypes(){
-		List typeList = new Vector();
-		for (Iterator iter = myShelves.iterator(); iter.hasNext();){
-			ShelfImpl currentShelf = (ShelfImpl)(iter.next());
-			if (!typeList.contains(currentShelf.getPlantType()))
-				typeList.add(currentShelf.getPlantType());
-		}
-		String[] plantTypeArray = new String[typeList.size()];
-		return (String[])(typeList.toArray(plantTypeArray));
 	}
 
 	public Shelf[] getShelves(){
@@ -176,52 +90,6 @@ public class BiomassRSImpl extends SimBioModuleImpl implements BiomassRSOperatio
 			theShelfArray[i] = ShelfHelper.narrow(OrbUtils.poaToCorbaObj(currentShelf));
 		}
 		return theShelfArray;
-	}
-
-	private void setProductionRate(float pProductionRate){
-		for (Iterator iter = myShelves.iterator(); iter.hasNext();){
-			ShelfImpl currentShelf = (ShelfImpl)(iter.next());
-			currentShelf.setProductionRate(pProductionRate);
-		}
-	}
-
-	protected String getMalfunctionName(MalfunctionIntensity pIntensity, MalfunctionLength pLength){
-		String returnName = new String();
-		if (pIntensity == MalfunctionIntensity.SEVERE_MALF)
-			returnName += "Severe ";
-		else if (pIntensity == MalfunctionIntensity.MEDIUM_MALF)
-			returnName += "Medium ";
-		else if (pIntensity == MalfunctionIntensity.LOW_MALF)
-			returnName += "Low ";
-		if (pLength == MalfunctionLength.TEMPORARY_MALF)
-			returnName += "Production Rate Decrease (temporary)";
-		else if (pLength == MalfunctionLength.PERMANENT_MALF)
-			returnName += "Production Rate Decrease (permanent)";
-		return returnName;
-	}
-
-	private void performMalfunctions(){
-		float productionRate = 1f;
-		for (Iterator iter = myMalfunctions.values().iterator(); iter.hasNext(); ){
-			Malfunction currentMalfunction = (Malfunction)(iter.next());
-			if (currentMalfunction.getLength() == MalfunctionLength.TEMPORARY_MALF){
-				if (currentMalfunction.getIntensity() == MalfunctionIntensity.SEVERE_MALF)
-					productionRate *= 0.50;
-				else if (currentMalfunction.getIntensity() == MalfunctionIntensity.MEDIUM_MALF)
-					productionRate *= 0.25;
-				else if (currentMalfunction.getIntensity() == MalfunctionIntensity.LOW_MALF)
-					productionRate *= 0.10;
-			}
-			else if (currentMalfunction.getLength() == MalfunctionLength.PERMANENT_MALF){
-				if (currentMalfunction.getIntensity() == MalfunctionIntensity.SEVERE_MALF)
-					productionRate *= 0.50;
-				else if (currentMalfunction.getIntensity() == MalfunctionIntensity.MEDIUM_MALF)
-					productionRate *= 0.25;
-				else if (currentMalfunction.getIntensity() == MalfunctionIntensity.LOW_MALF)
-					productionRate *= 0.10;
-			}
-		}
-		setProductionRate(productionRate);
 	}
 
 	/**
@@ -235,15 +103,7 @@ public class BiomassRSImpl extends SimBioModuleImpl implements BiomassRSOperatio
 		}
 	}
 
-	/**
-	* When ticked, the Biomass RS does the following
-	* on the condition that the plants aren't dead it.
-	* 1) attempts to collect references to various server (if not already done).
-	* 4) consumes
-	*/
 	public void tick(){
-		if (isMalfunctioning())
-			performMalfunctions();
 		for (Iterator iter = myShelves.iterator(); iter.hasNext();){
 			ShelfImpl currentShelf = (ShelfImpl)(iter.next());
 			currentShelf.tick();
