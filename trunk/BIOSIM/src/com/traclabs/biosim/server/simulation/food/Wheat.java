eb7ace2127cf22c8aa2a5052a0f4c6db8ad699fe
scott <scott@4576cf88-1eed-0310-b270-a9f98363dfe6>
2003-05-06 19:41:53 +0000

    none

--- trunk/BIOSIM/src/com/traclabs/biosim/server/simulation/food/Wheat.java
+++ trunk/BIOSIM/src/com/traclabs/biosim/server/simulation/food/Wheat.java
@@ -15,7 +15,7 @@ public class Wheat extends PlantImpl{
 		canopyClosureConstants[10] = 0.3419f;
 		canopyClosureConstants[11] = .00019733f;
 		canopyClosureConstants[15] = -.00019076f;
-		
+
 		canopyQYConstants[6] = .044793f;
 		canopyQYConstants[7] = .000051583f;
 		canopyQYConstants[8] = -.000000020724f;
@@ -27,67 +27,72 @@ public class Wheat extends PlantImpl{
 	public PlantType getPlantType(){
 		return PlantType.WHEAT;
 	}
-	
+
 	public float getPPFNeeded(){
 		return 4791667f;
 	}
-	
+
 	protected float getBCF(){
 		return 0.44f;
 	}
-	
+
 	protected float getCarbonUseEfficiency24(){
 		return 0.64f;
 	}
-	
+
 	protected float getPhotoperiod(){
 		return 20f;
 	}
-	
+
 	protected float getN(){
 		return 1.0f;
 	}
-	
+
 	protected float getCQYMin(){
 		return .001f;
 	}
-	
+
 	protected float getTimeTillCanopySenescence(){
 		return 33f;
 	}
-	
+
 	protected float getTimeTillCropMaturity(){
 		return 62f;
 	}
-	
+
 	protected float getOPF(){
 		return 1.07f;
 	}
-	
+
 	protected float getFreshFactor(){
 		return 6.4546f;
 	}
-	
+
 	public float getFractionOfEdibleBiomass(){
 		if (myAge > 34)
 			return 1f;
 		else
 			return 0f;
 	}
-	
+
 	protected float getEdibleFreshBasisWaterContent(){
 		return 12f;
 	}
-	
+
 	protected float getInedibleFreshBasisWaterContent(){
 		return 90f;
 	}
-	
+
 	protected float calculateCanopyStomatalConductance(){
 		float relativeHumdity = myShelfImpl.getBiomassRSImpl().getAirOutputs()[0].getRelativeHumidity();
-		return 0.1389f + 15.32f * relativeHumdity * (calculateNetCanopyPhotosynthesis() / calculateCO2Concentration());
+		float netCanopyPhotosynthesis = calculateNetCanopyPhotosynthesis();
+		float CO2Concentration = calculateCO2Concentration();
+		System.out.println("Wheat: relativeHumdity: "+relativeHumdity);
+		System.out.println("Wheat: netCanopyPhotosynthesis: "+netCanopyPhotosynthesis);
+		System.out.println("Wheat: CO2Concentration: "+CO2Concentration);;
+		return 0.1389f + 15.32f * relativeHumdity * (netCanopyPhotosynthesis / CO2Concentration);
 	}
-	
+
 	protected float calculateAtmosphericAeroDynamicConductance(){
 		return 5.5f;
 	}
