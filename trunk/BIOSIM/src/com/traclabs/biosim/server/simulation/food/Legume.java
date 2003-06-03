98eba1f14b28d071311fa6d5370ba30ef2105b5c
scott <scott@4576cf88-1eed-0310-b270-a9f98363dfe6>
2003-06-03 15:41:30 +0000

    none

---
+++ trunk/BIOSIM/src/com/traclabs/biosim/server/simulation/food/Legume.java
@@ -0,0 +1,53 @@
+package biosim.server.simulation.food;
+
+import biosim.idl.simulation.food.*;
+/**
+ * Legume
+ * @author    Scott Bell
+ */
+
+public abstract class Legume extends Planophile{
+	protected abstract float getBCF();
+	protected abstract float getPhotoperiod();
+	protected abstract float getN();
+	protected abstract float getTimeTillCanopySenescence();
+	protected abstract float getCQYMin();
+	protected abstract float getTimeTillCropMaturity();
+	protected abstract float getOPF();
+	protected abstract float getFreshFactor();
+	protected abstract float getFractionOfEdibleBiomass();
+	protected abstract float getEdibleFreshBasisWaterContent();
+	protected abstract float getInedibleFreshBasisWaterContent();
+	protected abstract float getCUEMax();
+	protected abstract float getCUEMin();
+	public abstract float getPPFNeeded();
+	public abstract PlantType getPlantType();
+	
+	public Legume(ShelfImpl pShelfImpl){
+		super(pShelfImpl);
+	}
+
+	protected float getCarbonUseEfficiency24(){
+		float CUEMax = getCUEMax();
+		float timeTillCanopySenescence = getTimeTillCanopySenescence();
+		System.out.println("Legume: CUEMax: "+CUEMax);
+		System.out.println("Legume: timeTillCanopySenescence: "+timeTillCanopySenescence);
+		if (getDaysOfGrowth() < getTimeTillCanopySenescence()){
+			return CUEMax; 
+		}
+		else{
+			float CUEMin = getCUEMin();
+			float daysOfGrowth = getDaysOfGrowth();
+			float timeTillCropMaturity = getTimeTillCropMaturity();
+			float calculatedCUE24 = CUEMax - ((CUEMax - CUEMin) * ((daysOfGrowth - timeTillCanopySenescence)) / (timeTillCropMaturity - timeTillCanopySenescence));
+			System.out.println("Legume: CUEMin: "+CUEMin);
+			System.out.println("Legume: daysOfGrowth: "+daysOfGrowth);
+			System.out.println("Legume: timeTillCropMaturity: "+timeTillCropMaturity);
+			if (calculatedCUE24 < 0f)
+				return 0f;
+			else
+				return calculatedCUE24;
+		}
+	}
+
+}
