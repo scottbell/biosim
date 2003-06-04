bc71a8c004c7b79fbbaf5152bdd9cb08030b2d64
scott <scottbell@users.noreply.github.com>
2003-06-04 20:37:23 +0000

    *** empty log message ***

---
+++ BIOSIM/src/com/traclabs/biosim/server/sensor/crew/CrewGroupDeathSensorImpl.java
@@ -0,0 +1,30 @@
+package biosim.server.sensor.crew;
+
+import biosim.server.sensor.framework.*;
+import biosim.idl.sensor.crew.*;
+import biosim.idl.framework.*;
+
+public class CrewGroupDeathSensorImpl extends CrewGroupSensorImpl implements CrewGroupDeathSensorOperations{
+	public CrewGroupDeathSensorImpl(int pID){
+		super(pID);
+	}
+
+	protected void gatherData(){
+		boolean preFilteredBooleanValue = getInput().isDead();
+		if (preFilteredBooleanValue)
+			myValue = randomFilter(1);
+		else
+			myValue = randomFilter(0);
+	}
+	
+	protected void notifyListeners(){
+	}
+	
+	/**
+	* Returns the name of this module (CrewGroupDeathSensor)
+	* @return the name of the module
+	*/
+	public String getModuleName(){
+		return "CrewGroupDeathSensor"+getID();
+	}
+}
---
+++ BIOSIM/src/com/traclabs/biosim/server/sensor/crew/CrewGroupSensorImpl.java
@@ -0,0 +1,33 @@
+package biosim.server.sensor.crew;
+
+import biosim.server.sensor.framework.*;
+import biosim.idl.sensor.crew.*;
+import biosim.idl.framework.*;
+import biosim.idl.simulation.crew.*;
+
+public abstract class CrewGroupSensorImpl extends GenericSensorImpl implements CrewGroupSensorOperations{
+	protected CrewGroup myCrewGroup;
+	
+	public CrewGroupSensorImpl(int pID){
+		super(pID);
+	}
+
+	protected abstract void gatherData();
+	protected abstract void notifyListeners();
+
+	public void setInput(CrewGroup source){
+		myCrewGroup = source;
+	}
+	
+	public CrewGroup getInput(){
+		return myCrewGroup;
+	}
+	
+	/**
+	* Returns the name of this module (CrewGroupSensor)
+	* @return the name of the module
+	*/
+	public String getModuleName(){
+		return "CrewGroupSensor"+getID();
+	}
+}
