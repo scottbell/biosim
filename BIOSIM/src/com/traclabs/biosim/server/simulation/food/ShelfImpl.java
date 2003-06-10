7399dcbe39178fc64ad0d19d7f7bece546b3d3f6
scott <scottbell@users.noreply.github.com>
2003-06-10 20:30:03 +0000

    none

--- BIOSIM/src/com/traclabs/biosim/server/simulation/food/ShelfImpl.java
+++ BIOSIM/src/com/traclabs/biosim/server/simulation/food/ShelfImpl.java
@@ -18,7 +18,7 @@ public class ShelfImpl extends ShelfPOA {
 	private boolean logInitialized = false;
 	private BiomassRSImpl myBiomassRSImpl;
 	private float waterLevel = 0f;
-	private static final float waterNeededPerMeterSquared = 2f; //grab up to 2 liters per meters squared of crops per hour(WAG)
+	private static final float waterNeededPerMeterSquared = 0.2f; //grab up to 2 liters per meters squared of crops per hour(WAG)
 	private float waterNeeded = 0f;;
 	private float powerLevel = 0f;
 	
