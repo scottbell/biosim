52ac790a5dfc6be6ab2a3241cfe6c9cc4ad2c4a0
scott <scott@4576cf88-1eed-0310-b270-a9f98363dfe6>
2003-09-30 18:24:45 +0000

    none

---
+++ trunk/BIOSIM/src/com/traclabs/biosim/server/actuator/environment/NitrogenAirEnvironmentOutFlowRateActuatorImpl.java
@@ -0,0 +1,45 @@
+package biosim.server.actuator.environment;
+
+import biosim.server.actuator.framework.*;
+import biosim.idl.actuator.environment.*;
+import biosim.idl.simulation.environment.*;
+import biosim.idl.framework.*;
+
+public class NitrogenAirEnvironmentOutFlowRateActuatorImpl extends GenericActuatorImpl implements NitrogenAirEnvironmentOutFlowRateActuatorOperations{
+	private NitrogenAirProducer myProducer;
+	private int myIndex;
+	
+	public NitrogenAirEnvironmentOutFlowRateActuatorImpl(int pID, String pName){
+		super(pID, pName);
+	}
+
+	protected void processData(){
+		float myFilteredValue = randomFilter(myValue);
+		getOutput().setNitrogenAirEnvironmentOutputDesiredFlowRate(myFilteredValue, myIndex);
+	}
+	
+	protected void notifyListeners(){
+		//does nothing right now
+	}
+
+	public void setOutput(NitrogenAirProducer pProducer, int pIndex){
+		myProducer = pProducer;
+		myIndex = pIndex;
+	}
+	
+	protected BioModule getOutputModule(){
+		return (BioModule)(myProducer);
+	}
+	
+	public float getMax(){
+		return myProducer.getNitrogenAirEnvironmentOutputMaxFlowRate(myIndex);
+	}
+	
+	public NitrogenAirProducer getOutput(){
+		return myProducer;
+	}
+	
+	public int getIndex(){
+		return myIndex;
+	}
+}
---
+++ trunk/BIOSIM/src/com/traclabs/biosim/server/actuator/environment/WaterAirEnvironmentInFlowRateActuatorImpl.java
@@ -0,0 +1,45 @@
+package biosim.server.actuator.environment;
+
+import biosim.server.actuator.framework.*;
+import biosim.idl.actuator.environment.*;
+import biosim.idl.simulation.environment.*;
+import biosim.idl.framework.*;
+
+public class WaterAirEnvironmentInFlowRateActuatorImpl extends GenericActuatorImpl implements WaterAirEnvironmentInFlowRateActuatorOperations{
+	private WaterAirConsumer myConsumer;
+	private int myIndex;
+	
+	public WaterAirEnvironmentInFlowRateActuatorImpl(int pID, String pName){
+		super(pID, pName);
+	}
+
+	protected void processData(){
+		float myFilteredValue = randomFilter(myValue);
+		getOutput().setWaterAirEnvironmentInputDesiredFlowRate(myFilteredValue, myIndex);
+	}
+	
+	protected void notifyListeners(){
+		//does nothing right now
+	}
+
+	public void setOutput(WaterAirConsumer pConsumer, int pIndex){
+		myConsumer = pConsumer;
+		myIndex = pIndex;
+	}
+	
+	public WaterAirConsumer getOutput(){
+		return myConsumer;
+	}
+	
+	protected BioModule getOutputModule(){
+		return (BioModule)(myConsumer);
+	}
+	
+	public int getIndex(){
+		return myIndex;
+	}
+	
+	public float getMax(){
+		return myConsumer.getWaterAirEnvironmentInputMaxFlowRate(myIndex);
+	}
+}
