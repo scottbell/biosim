4709937bbee595e3e6971de14c3485af9377109d
scott <scott@4576cf88-1eed-0310-b270-a9f98363dfe6>
2003-10-03 19:57:56 +0000

    Full size images

---
+++ trunk/BIOSIM/src/com/traclabs/biosim/server/sensor/crew/CrewGroupProductivitySensorImpl.java
@@ -0,0 +1,27 @@
+package biosim.server.sensor.crew;
+
+import biosim.server.sensor.framework.*;
+import biosim.idl.sensor.crew.*;
+import biosim.idl.framework.*;
+
+public class CrewGroupProductivitySensorImpl extends CrewGroupSensorImpl implements CrewGroupProductivitySensorOperations{
+	public CrewGroupProductivitySensorImpl(int pID, String pName){
+		super(pID, pName);
+	}
+
+	protected void gatherData(){
+		float preFilteredValue = getInput().getProductivity();
+		myValue = randomFilter(preFilteredValue);
+	}
+	
+	public float getMax(){
+		return Float.MAX_VALUE;
+	}
+	
+	protected void notifyListeners(){
+	}
+	
+	protected BioModule getInputModule(){
+		return (BioModule)(getInput());
+	}
+}
---
+++ trunk/BIOSIM/src/com/traclabs/biosim/server/simulation/air/NitrogenStoreServer.java
@@ -0,0 +1,22 @@
+package biosim.server.simulation.air;
+
+import biosim.idl.simulation.air.*;
+import biosim.server.framework.*;
+/**
+ * The Nitrogen Store Server.  Creates an instance of the NitrogenStore and registers it with the nameserver.
+ *
+ * @author    Scott Bell
+ */
+public class NitrogenStoreServer extends GenericServer{
+	
+	/**
+	* Instantiates the server and binds it to the name server.
+	* @param args aren't used for anything
+	*/
+	public static void main(String args[]) {
+		NitrogenStoreServer myServer = new NitrogenStoreServer();
+		NitrogenStoreImpl myNitrogenStore = new NitrogenStoreImpl(myServer.getIDfromArgs(args), myServer.getNamefromArgs(args));
+		myServer.registerServerAndRun(new NitrogenStorePOATie(myNitrogenStore), myNitrogenStore.getModuleName(), myNitrogenStore.getID());
+	}
+}
+
