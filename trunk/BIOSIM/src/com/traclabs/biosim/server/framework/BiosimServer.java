f7360439bf70504fc949b2db12cb7bddf1c106e0
scott <scott@4576cf88-1eed-0310-b270-a9f98363dfe6>
2003-10-21 21:15:13 +0000

    none

--- trunk/BIOSIM/src/com/traclabs/biosim/server/framework/BiosimServer.java
+++ trunk/BIOSIM/src/com/traclabs/biosim/server/framework/BiosimServer.java
@@ -14,8 +14,13 @@ import biosim.server.util.*;
 
 public class BiosimServer extends GenericServer{
 	
-	public BiosimServer(int id, int stutterLength){
-		URL documentUrl = ClassLoader.getSystemClassLoader().getResource("biosim/server/framework/DefaultInitialization.xml");
+	public BiosimServer(int id, int stutterLength, String xmlLocation){
+		URL documentUrl = ClassLoader.getSystemClassLoader().getResource(xmlLocation);
+		if (documentUrl == null){
+			System.err.println("Couldn't find init xml file: "+xmlLocation);
+			System.err.println("Exiting...");
+			System.exit(1);
+		}
 		LoggerImpl myLoggerImpl = new LoggerImpl(id);
 		myLoggerImpl.addLogHandlerType(LogHandlerType.XML);
 		BioDriverImpl myBioDriverImpl = new BioDriverImpl(id);
@@ -34,7 +39,8 @@ public class BiosimServer extends GenericServer{
 	*/
 	public static void main(String args[]) {
 		int id = BiosimServer.getIDfromArgs(args);
-		BiosimServer server = new BiosimServer(id, 0);
+		String xmlLocation = BiosimServer.getXMLfromArgs(args);
+		BiosimServer server = new BiosimServer(id, 0, xmlLocation);
 		server.runServer("BiosimServer (id="+id+")");
 	}
 }
