f46e86fbed800cb9dacc1ae44e6f67f8deaf37a4
scott <scottbell@users.noreply.github.com>
2003-10-22 16:46:57 +0000

    none

---
+++ BIOSIM/src/com/traclabs/biosim/server/actuator/food/PlantingActuatorImpl.java
@@ -0,0 +1,32 @@
+package biosim.server.actuator.food;
+
+import biosim.server.actuator.framework.*;
+import biosim.idl.actuator.food.*;
+import biosim.idl.framework.*;
+import biosim.idl.simulation.food.*;
+
+public class PlantingActuatorImpl extends ShelfActuatorImpl implements PlantingActuatorOperations{
+	private PlantType myType = PlantType.UNKNOWN_PLANT;
+	
+	public PlantingActuatorImpl(int pID, String pName){
+		super(pID, pName);
+	}
+
+	protected void processData(){
+		float myFilteredValue = randomFilter(myValue);
+		//replant crops
+	}
+	
+	protected void notifyListeners(){
+		//does nothing right now
+	}
+	
+	public float getMax(){
+		return myShelf.getCropArea();
+	}
+	
+	public void setPlantType(PlantType pType){
+		myType = pType;
+		newValue = true;
+	}
+}
