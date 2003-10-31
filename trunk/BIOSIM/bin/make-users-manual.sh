861a0590476fcc1daae01ac0771ba2e64bb85871
scott <scott@4576cf88-1eed-0310-b270-a9f98363dfe6>
2003-10-31 23:26:01 +0000

    none

--- trunk/BIOSIM/bin/make-users-manual.sh
+++ trunk/BIOSIM/bin/make-users-manual.sh
@@ -1,35 +1,55 @@
 #!/bin/bash
 
+if [ "$1" == "-?" ]; then
+	echo -e "usage: make-users-manual.sh [-debug] [-show] [-?]"
+	exit
+fi
 echo "*building users manual"
 # see if the biosim directory exists, if it doesn't, assume it's one directory back (i.e. user is in bin directory)
 devRootDir=$BIOSIM_HOME
 currentDir=`pwd`
+show="false"
+textOutput=/dev/null
 if [ -z "$devRootDir" ]
 then
 	cd ..
 	devRootDir=`pwd`
 	cd $currentDir
-	echo "		-assuming BIOSIM_HOME is $devRootDir"
+	echo "	-assuming BIOSIM_HOME is $devRootDir"
+fi
+if [ "$1" == "-debug" ]; then
+	echo "	-debug mode on"
+	textOutput=/dev/tty
+	if [ "$2" == "-show" ]; then
+		show="true"
+	fi
+fi
+if [ "$1" == "-show" ]; then
+	show="true"
+	if [ "$2" == "-debug" ]; then
+		echo "	-debug mode on"
+		textOutput=/dev/tty
+	fi
 fi
 userManDir="$devRootDir/doc/users_manual_files"
 cd $userManDir
-echo "		-creating empty index file"
-touch users_manual.ind > /dev/null
-echo "		-first latex pass"
-latex users_manual.tex > /dev/null
-echo "		-making index file"
-makeindex users_manual.idx 2> /dev/null
-echo "		-making bibliography"
-bibtex users_manual.tex > /dev/null
-echo "		-second latex pass"
-latex users_manual.tex > /dev/null
-echo "		-creating ps file"
-dvips -o users_manual.ps users_manual.dvi 2> /dev/null
-echo "		-creating pdf file"
-ps2pdf users_manual.ps > /dev/null
-if [ "$1" == "show" ]; then
-	echo "		-launching kghostview"
-	kghostview users_manual.pdf &  > /dev/null
+echo "	-creating empty index file"
+touch users_manual.ind > $textOutput
+echo "	-first latex pass"
+latex users_manual.tex > $textOutput
+echo "	-making index file"
+makeindex users_manual.idx 2> $textOutput
+echo "	-making bibliography"
+bibtex users_manual.tex > $textOutput
+echo "	-second latex pass"
+latex users_manual.tex > $textOutput
+echo "	-creating ps file"
+dvips -o users_manual.ps users_manual.dvi 2> $textOutput
+echo "	-creating pdf file"
+ps2pdf users_manual.ps > $textOutput
+if [ "$show" == "true" ]; then
+	echo "	-launching kghostview"
+	kghostview users_manual.pdf &  > $textOutput
 fi
 cd $currentDir
 echo "*done users manual"
