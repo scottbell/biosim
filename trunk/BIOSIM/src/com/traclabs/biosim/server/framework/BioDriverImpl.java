845fb3aacd329dd6efe9e4479a6b7203c1c283e7
scott <scott@4576cf88-1eed-0310-b270-a9f98363dfe6>
2003-12-08 22:36:33 +0000

    none

--- trunk/BIOSIM/src/com/traclabs/biosim/server/framework/BioDriverImpl.java
+++ trunk/BIOSIM/src/com/traclabs/biosim/server/framework/BioDriverImpl.java
@@ -433,7 +433,7 @@ public class BioDriverImpl extends BioDriverPOA{
 	*/
 	private void tick(){
 		if (!isStarted()){
-			System.out.println("Tick called when simulation wasn't started!");
+			System.err.println("Tick called when simulation wasn't started!");
 			return;
 		}
 		//Iterate through the actuators and tick them
@@ -444,6 +444,7 @@ public class BioDriverImpl extends BioDriverPOA{
 		//Iterate through the sim modules and tick them
 		for (int i = 0; i < simModules.length; i++){
 			BioModule currentBioModule = (BioModule)(simModules[i]);
+			System.out.println("Ticking "+currentBioModule.getModuleName());
 			currentBioModule.tick();
 		}
 		//Iterate through the sensors and tick them
