6db76b9497b788613848cffad4703ae350c4a8f6
scott <scottbell@users.noreply.github.com>
2004-07-13 20:06:14 +0000

    *** empty log message ***

--- BIOSIM/src/com/traclabs/biosim/server/simulation/environment/SimEnvironmentImpl.java
+++ BIOSIM/src/com/traclabs/biosim/server/simulation/environment/SimEnvironmentImpl.java
@@ -88,7 +88,7 @@ public class SimEnvironmentImpl extends SimBioModuleImpl implements SimEnvironme
 		myName = pName;
 		volume = initialVolume = pInitialVolume;
 		O2Pressure = cachedO2Pressure = initialO2Pressure = 20.0f;
-		CO2Pressure = cachedCO2Pressure = initialCO2Pressure = 0.12f;
+		CO2Pressure = cachedCO2Pressure = initialCO2Pressure = 0.111f;
 		otherPressure = cachedOtherPressure = initialOtherPressure = 1.0f;
 		nitrogenPressure = cachedNitrogenPressure = initialNitrogenPressure = 78.96f;
 		waterPressure = cachedWaterPressure = initialWaterPressure = 1.0f;
