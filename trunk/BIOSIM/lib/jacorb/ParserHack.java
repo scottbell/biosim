65d1225b569aadd3540e203d70d1a736a716b50b
scott <scott@4576cf88-1eed-0310-b270-a9f98363dfe6>
2004-08-11 23:55:04 +0000

    *** empty log message ***

---
+++ trunk/BIOSIM/lib/jacorb/ParserHack.java
@@ -0,0 +1,14 @@
+import org.jacorb.idl.parser;
+
+/*
+ * Workaround to ghetto System.exit call in Jacorb's main method.
+ * @author    Scott Bell
+ */
+
+public class ParserHack{
+	public static void main(String args[]){
+		parser myParser = new parser();
+		myParser.compileAndHandle(args);
+	}
+}
+
