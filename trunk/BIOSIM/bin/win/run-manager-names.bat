4d4b891dee9759fa90c28bf6b6cae7f34e277017
scott <scott@4576cf88-1eed-0310-b270-a9f98363dfe6>
2004-09-14 15:24:05 +0000

    *** empty log message ***

--- trunk/BIOSIM/bin/win/run-manager-names.bat
+++ trunk/BIOSIM/bin/win/run-manager-names.bat
@@ -1,2 +1,9 @@
 @echo off
-bash %BIOSIM_HOME%/bin/run-manager-names.sh %1 %2
+set mainClass=org.jacorb.naming.namemanager.NameManager
+set jacoOrbClass=org.omg.CORBA.ORBClass=org.jacorb.orb.ORB
+set jacoSingletonOrbClass=org.omg.CORBA.ORBSingletonClass=org.jacorb.orb.ORBSingleton
+set jacoNameIOR=ORBInitRef.NameService=file:%BIOSIM_HOME%\tmp\ns\ior.txt
+set resourceDir=%BIOSIM_HOME%\resources
+set jacoClasspath=%BIOSIM_HOME%\lib\jacorb\jacorb.jar;%BIOSIM_HOME%\lib\jacorb;%BIOSIM_HOME%\lib\jacorb\avalon-framework.jar;%BIOSIM_HOME%\lib\jacorb\logkit.jar
+java -classpath %jacoClasspath% -D%jacoOrbClass% -D%jacoSingletonOrbClass% -D%jacoNameIOR% %mainClass%
+pause
