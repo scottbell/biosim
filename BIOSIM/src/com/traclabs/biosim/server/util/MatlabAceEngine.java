2452ee4d07947419fb65f16a48dcbd99838d456c
scott <scottbell@users.noreply.github.com>
2004-10-27 23:46:08 +0000

    *** empty log message ***

--- BIOSIM/src/com/traclabs/biosim/server/util/MatlabAceEngine.java
+++ BIOSIM/src/com/traclabs/biosim/server/util/MatlabAceEngine.java
@@ -1,7 +1,22 @@
 package com.traclabs.biosim.server.util;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class MatlabAceEngine extends Engine {
+	private Socket mySocket;
+	
+	private static final String DEFAULT_HOSTNAME = "localhost";
+	private static final int DEFAULT_PORT = 12759;
+	
     public void open() {
+    	try{
+    		mySocket = new Socket(DEFAULT_HOSTNAME, DEFAULT_PORT);
+    	}
+    	catch (IOException e){
+    		myLogger.error(e);
+    		myLogger.error("Couldn't connect to "+DEFAULT_HOSTNAME+" on port "+DEFAULT_PORT);
+    	}
     }
 
     public void put(double[] inputVector) {
@@ -15,5 +30,12 @@ public class MatlabAceEngine extends Engine {
     }
 
     public void close() {
+    	try{
+    		mySocket = new Socket(DEFAULT_HOSTNAME, DEFAULT_PORT);
+    	}
+    	catch (IOException e){
+    		myLogger.error(e);
+    		myLogger.error("Couldn't connect to "+DEFAULT_HOSTNAME+" on port "+DEFAULT_PORT);
+    	}
     }
 }
