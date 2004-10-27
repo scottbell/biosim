89a57db6624d3002e78a172de9ae5eda1dc88254
scott <scott@4576cf88-1eed-0310-b270-a9f98363dfe6>
2004-10-27 22:06:45 +0000

    *** empty log message ***

---
+++ trunk/BIOSIM/bin/run-backend-test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+userSelect="$@"
+devRootDir=$BIOSIM_HOME
+currentDir=`pwd`
+if [ -z "$devRootDir" ]
+then
+	cd ..
+	devRootDir=`pwd`
+	cd $currentDir
+	echo "		-assuming BIOSIM_HOME is $devRootDir"
+fi
+java_command=$JAVA_HOME/bin/java
+if [ -z "$JAVA_HOME" ]
+then
+	echo "		-JAVA_HOME not set, assuming java and javac are in path..."
+	java_command="java"
+fi
+####################
+# CLIENTS START	   #
+####################
+testClasspath="$devRootDir/lib/log4j/log4j.jar:$devRootDir/build"
+javaInvocation="$java_command -classpath $testClasspath"
+backendProgram="com.traclabs.biosim.server.util.MatlabTestBackend"
+$javaInvocation $backendProgram
+
+
+
--- trunk/BIOSIM/src/com/traclabs/biosim/server/util/MatlabTestBackend.java
+++ trunk/BIOSIM/src/com/traclabs/biosim/server/util/MatlabTestBackend.java
@@ -4,26 +4,30 @@
  */
 package com.traclabs.biosim.server.util;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.InetSocketAddress;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Properties;
 
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 /**
  * @author Scott Bell
  */
 public class MatlabTestBackend {
-	
+	private static final int DEFAULT_SERVER_PORT = 12759;
 	private ServerSocket myIncomingSocket;
-	private Socket myOutgoingSocket;
-	private InetSocketAddress myAddressToConnectTo;
 	private Logger myLogger;
 	
-	public MatlabTestBackend(int pPortToListen, InetSocketAddress pAddressToConnect){
-		myLogger = Logger.getLogger(this.getClass());
-		myAddressToConnectTo = pAddressToConnect;
+	public MatlabTestBackend(int pPortToListen){
+		myLogger = Logger.getLogger(this.getClass().toString());
 		try{
 			myIncomingSocket = new ServerSocket(pPortToListen);
 		}
@@ -33,18 +37,61 @@ public class MatlabTestBackend {
 		}
 	}
 	
-	private void createOutgoingSocket(){
-		try{
-			myOutgoingSocket = new Socket(myAddressToConnectTo.getAddress(), myAddressToConnectTo.getPort());
-		}
-		catch (IOException e){
-			myLogger.error("Couldn't connect to biosim!");
-			myLogger.error(e);
-		}
-	}
-	
 	public static void main(String args[]){
-		
+		Properties logProps = new Properties();
+        logProps.setProperty("log4j.appender.matlabAppender",
+                "org.apache.log4j.ConsoleAppender");
+        logProps.setProperty("log4j.appender.matlabAppender.layout",
+                "org.apache.log4j.PatternLayout");
+        logProps.setProperty(
+                "log4j.appender.matlabAppender.layout.ConversionPattern",
+                "%5p [%c] - %m%n");
+        logProps.setProperty("log4j.logger." + MatlabTestBackend.class,
+                "DEBUG, matlabAppender");
+        PropertyConfigurator.configure(logProps);
+        
+		int serverPort = DEFAULT_SERVER_PORT;
+		if (args.length > 1){
+			Logger.getLogger(MatlabTestBackend.class).error("usage: java com.traclabs.biosim.server.util.MatlabTestBackend [server port]");
+			return;
+		}
+		else if (args.length == 1){
+			try{
+				serverPort = Integer.parseInt(args[0]);
+			}
+			catch (NumberFormatException e){
+				Logger.getLogger(MatlabTestBackend.class).error(e);
+				Logger.getLogger(MatlabTestBackend.class).error("Had problems parsing server port you specified: "+args[0]);
+				return;
+			}
+		}
+		MatlabTestBackend newMatlabTestBackend = new MatlabTestBackend(serverPort);
+		newMatlabTestBackend.startServer();
+	}
+
+	/**
+	 * 
+	 */
+	private void startServer() {
+		myLogger.info("Starting server...");
+			while (true){
+				try{
+					Socket clientSocket = myIncomingSocket.accept();
+					myLogger.info("Client connected!");
+					BufferedReader socketTextReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())) ;
+					DataInputStream  socketInputDataStream = new DataInputStream(clientSocket.getInputStream());
+				    PrintWriter socketTextWriter = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream(), "8859_1"), true );
+				    DataOutputStream socketOutputDataStream =  new DataOutputStream(clientSocket.getOutputStream());
+				    
+				    socketTextWriter.print("Test!");
+				    float readFloat = socketInputDataStream.readFloat();
+				    //send it back multiplied by 2
+				    socketOutputDataStream.writeFloat(readFloat * 2f);
+				}
+				catch (IOException e){
+					myLogger.error(e);
+				}
+			}
 	}
 
 }
