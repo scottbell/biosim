4c1e58c22e9d6226cbd4028825b273458611175c
scott <scott@4576cf88-1eed-0310-b270-a9f98363dfe6>
2004-11-22 19:10:13 +0000

    *** empty log message ***

---
+++ trunk/BIOSIM/src/com/traclabs/biosim/server/simulation/air/OGSImpl.java
@@ -0,0 +1,288 @@
+package com.traclabs.biosim.server.simulation.air;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+import com.traclabs.biosim.idl.framework.Malfunction;
+import com.traclabs.biosim.idl.framework.MalfunctionIntensity;
+import com.traclabs.biosim.idl.framework.MalfunctionLength;
+import com.traclabs.biosim.idl.simulation.air.AirRSOperationMode;
+import com.traclabs.biosim.idl.simulation.air.AirRSOperations;
+import com.traclabs.biosim.idl.simulation.air.CO2Store;
+import com.traclabs.biosim.idl.simulation.air.H2Store;
+import com.traclabs.biosim.idl.simulation.air.O2Store;
+import com.traclabs.biosim.idl.simulation.environment.SimEnvironment;
+import com.traclabs.biosim.idl.simulation.framework.H2ProducerOperations;
+import com.traclabs.biosim.idl.simulation.framework.O2ProducerOperations;
+import com.traclabs.biosim.idl.simulation.framework.PotableWaterConsumerOperations;
+import com.traclabs.biosim.idl.simulation.framework.PowerConsumerOperations;
+import com.traclabs.biosim.idl.simulation.power.PowerStore;
+import com.traclabs.biosim.idl.simulation.water.PotableWaterStore;
+import com.traclabs.biosim.server.simulation.framework.SimBioModuleImpl;
+
+/**
+ * The Air Revitalization System Implementation. Takes in Air (O2, CO2, other)
+ * from the environment and power from the Power Production System and produces
+ * air with less CO2 and more O2.
+ * 
+ * @author Scott Bell
+ */
+
+public class OGSImpl extends SimBioModuleImpl implements AirRSOperations,
+        PowerConsumerOperations, PotableWaterConsumerOperations,
+        O2ProducerOperations, H2ProducerOperations{
+    
+    private O2Store[] myO2Stores;
+
+    private PowerStore[] myPowerStores;
+
+    private PotableWaterStore[] myPotableWaterStoreInputs;
+
+    private H2Store[] myH2OutputStores;
+
+    private float[] powerMaxFlowRates;
+
+    private float[] powerActualFlowRates;
+
+    private float[] powerDesiredFlowRates;
+
+    private float[] potableWaterInMaxFlowRates;
+
+    private float[] potableWaterInActualFlowRates;
+
+    private float[] potableWaterInDesiredFlowRates;
+
+    private float[] O2MaxFlowRates;
+
+    private float[] O2ActualFlowRates;
+
+    private float[] O2DesiredFlowRates;
+
+    private float[] H2OutputMaxFlowRates;
+
+    private float[] H2OutputActualFlowRates;
+
+    private float[] H2OutputDesiredFlowRates;
+
+    public OGSImpl(int pID, String pName) {
+        super(pID, pName);
+
+        myO2Stores = new O2Store[0];
+        myPowerStores = new PowerStore[0];
+        powerMaxFlowRates = new float[0];
+        powerActualFlowRates = new float[0];
+        powerDesiredFlowRates = new float[0];
+        potableWaterInMaxFlowRates = new float[0];
+        potableWaterInActualFlowRates = new float[0];
+        potableWaterInDesiredFlowRates = new float[0];
+        O2MaxFlowRates = new float[0];
+        O2ActualFlowRates = new float[0];
+        O2DesiredFlowRates = new float[0];
+        H2OutputMaxFlowRates = new float[0];
+        H2OutputActualFlowRates = new float[0];
+        H2OutputDesiredFlowRates = new float[0];
+    }
+
+    /**
+     * Resets production/consumption levels.
+     */
+    public void reset() {
+        super.reset();
+    }
+
+    //Power Inputs
+    public void setPowerInputMaxFlowRate(float watts, int index) {
+        powerMaxFlowRates[index] = watts;
+    }
+
+    public float getPowerInputMaxFlowRate(int index) {
+        return powerMaxFlowRates[index];
+    }
+
+    public float[] getPowerInputMaxFlowRates() {
+        return powerMaxFlowRates;
+    }
+
+    public void setPowerInputDesiredFlowRate(float watts, int index) {
+        powerDesiredFlowRates[index] = watts;
+    }
+
+    public float getPowerInputDesiredFlowRate(int index) {
+        return powerDesiredFlowRates[index];
+    }
+
+    public float[] getPowerInputDesiredFlowRates() {
+        return powerDesiredFlowRates;
+    }
+
+    public float getPowerInputActualFlowRate(int index) {
+        return powerActualFlowRates[index];
+    }
+
+    public float[] getPowerInputActualFlowRates() {
+        return powerActualFlowRates;
+    }
+
+    public void setPowerInputs(PowerStore[] sources, float[] maxFlowRates,
+            float[] desiredFlowRates) {
+        myPowerStores = sources;
+        powerMaxFlowRates = maxFlowRates;
+        powerDesiredFlowRates = desiredFlowRates;
+        powerActualFlowRates = new float[powerDesiredFlowRates.length];
+    }
+
+    public PowerStore[] getPowerInputs() {
+        return myPowerStores;
+    }
+
+    void addPowerInputActualFlowRate(float watts, int index) {
+        powerActualFlowRates[index] += watts;
+    }
+
+    //Potable Water Inputs
+    public void setPotableWaterInputMaxFlowRate(float watts, int index) {
+        potableWaterInMaxFlowRates[index] = watts;
+    }
+
+    public float getPotableWaterInputMaxFlowRate(int index) {
+        return potableWaterInMaxFlowRates[index];
+    }
+
+    public float[] getPotableWaterInputMaxFlowRates() {
+        return potableWaterInMaxFlowRates;
+    }
+
+    public void setPotableWaterInputDesiredFlowRate(float watts, int index) {
+        potableWaterInDesiredFlowRates[index] = watts;
+    }
+
+    public float getPotableWaterInputDesiredFlowRate(int index) {
+        return potableWaterInDesiredFlowRates[index];
+    }
+
+    public float[] getPotableWaterInputDesiredFlowRates() {
+        return potableWaterInDesiredFlowRates;
+    }
+
+    public float getPotableWaterInputActualFlowRate(int index) {
+        return potableWaterInActualFlowRates[index];
+    }
+
+    public float[] getPotableWaterInputActualFlowRates() {
+        return potableWaterInActualFlowRates;
+    }
+
+    public void setPotableWaterInputs(PotableWaterStore[] sources,
+            float[] maxFlowRates, float[] desiredFlowRates) {
+        myPotableWaterStoreInputs = sources;
+        potableWaterInMaxFlowRates = maxFlowRates;
+        potableWaterInDesiredFlowRates = desiredFlowRates;
+        potableWaterInActualFlowRates = new float[potableWaterInDesiredFlowRates.length];
+    }
+
+    public PotableWaterStore[] getPotableWaterInputs() {
+        return myPotableWaterStoreInputs;
+    }
+
+    void addPotableWaterInputActualFlowRate(float watts, int index) {
+        potableWaterInActualFlowRates[index] += watts;
+    }
+
+    //O2 Ouputs
+    public void setO2OutputMaxFlowRate(float moles, int index) {
+        O2MaxFlowRates[index] = moles;
+    }
+
+    public float getO2OutputMaxFlowRate(int index) {
+        return O2MaxFlowRates[index];
+    }
+
+    public float[] getO2OutputMaxFlowRates() {
+        return O2MaxFlowRates;
+    }
+
+    public void setO2OutputDesiredFlowRate(float moles, int index) {
+        O2DesiredFlowRates[index] = moles;
+    }
+
+    public float getO2OutputDesiredFlowRate(int index) {
+        return O2DesiredFlowRates[index];
+    }
+
+    public float[] getO2OutputDesiredFlowRates() {
+        return O2DesiredFlowRates;
+    }
+
+    public float getO2OutputActualFlowRate(int index) {
+        return O2ActualFlowRates[index];
+    }
+
+    public float[] getO2OutputActualFlowRates() {
+        return O2ActualFlowRates;
+    }
+
+    public void setO2Outputs(O2Store[] sources, float[] maxFlowRates,
+            float[] desiredFlowRates) {
+        myO2Stores = sources;
+        O2MaxFlowRates = maxFlowRates;
+        O2DesiredFlowRates = desiredFlowRates;
+        O2ActualFlowRates = new float[O2DesiredFlowRates.length];
+    }
+
+    public O2Store[] getO2Outputs() {
+        return myO2Stores;
+    }
+
+    void setO2OutputActualFlowRate(float moles, int index) {
+        O2ActualFlowRates[index] = moles;
+    }
+
+    //H2 Ouputs
+    public void setH2OutputMaxFlowRate(float moles, int index) {
+        H2OutputMaxFlowRates[index] = moles;
+    }
+
+    public float getH2OutputMaxFlowRate(int index) {
+        return H2OutputMaxFlowRates[index];
+    }
+
+    public float[] getH2OutputMaxFlowRates() {
+        return H2OutputMaxFlowRates;
+    }
+
+    public void setH2OutputDesiredFlowRate(float moles, int index) {
+        H2OutputDesiredFlowRates[index] = moles;
+    }
+
+    public float getH2OutputDesiredFlowRate(int index) {
+        return H2OutputDesiredFlowRates[index];
+    }
+
+    public float[] getH2OutputDesiredFlowRates() {
+        return H2OutputDesiredFlowRates;
+    }
+
+    public float getH2OutputActualFlowRate(int index) {
+        return H2OutputActualFlowRates[index];
+    }
+
+    public float[] getH2OutputActualFlowRates() {
+        return H2OutputActualFlowRates;
+    }
+
+    public void setH2Outputs(H2Store[] sources, float[] maxFlowRates,
+            float[] desiredFlowRates) {
+        myH2OutputStores = sources;
+        H2OutputMaxFlowRates = maxFlowRates;
+        H2OutputDesiredFlowRates = desiredFlowRates;
+        H2OutputActualFlowRates = new float[H2OutputDesiredFlowRates.length];
+    }
+
+    public H2Store[] getH2Outputs() {
+        return myH2OutputStores;
+    }
+
+    void setH2OutputActualFlowRate(float moles, int index) {
+        H2OutputActualFlowRates[index] = moles;
+    }
+}
