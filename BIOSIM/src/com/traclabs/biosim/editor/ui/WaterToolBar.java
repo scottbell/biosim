dbed230d9016e7e6a2df845d3a3812197328c27a
scott <scottbell@users.noreply.github.com>
2005-04-06 23:40:05 +0000

    added toolbars

---
+++ BIOSIM/src/com/traclabs/biosim/editor/ui/WaterToolBar.java
@@ -0,0 +1,43 @@
+/*
+ * Created on Jan 26, 2005
+ *
+ * TODO
+ */
+package com.traclabs.biosim.editor.ui;
+
+import javax.swing.JButton;
+
+/**
+ * @author scott
+ * 
+ * TODO
+ */
+public class WaterToolBar extends EditorToolBar {
+    private JButton myWaterRSButton;
+
+    private JButton myPotableWaterStoreButton;
+
+    private JButton myGreyWaterStoreButton;
+
+    private JButton myDirtyWaterStoreButton;
+
+    private JButton myConduitButton;
+
+    public WaterToolBar() {
+        super("Water");
+        myWaterRSButton = new JButton("Water RS");
+        myPotableWaterStoreButton = new JButton("Potable Water Store");
+        myGreyWaterStoreButton = new JButton("Grey Water Store");
+        myDirtyWaterStoreButton = new JButton("Dirty Water Store");
+        myConduitButton = new JButton("Conduit");
+
+        add(myWaterRSButton);
+        addSeparator();
+        add(myPotableWaterStoreButton);
+        add(myGreyWaterStoreButton);
+        add(myDirtyWaterStoreButton);
+        addSeparator();
+        add(myConduitButton);
+    }
+
+}
