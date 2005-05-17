9266d95947291bf2f21a13791125b1185715c4dd
scott <scott@4576cf88-1eed-0310-b270-a9f98363dfe6>
2005-05-17 19:07:47 +0000

    Added sensors to environments.

--- trunk/BIOSIM/src/com/traclabs/biosim/editor/graph/environment/SimEnvironmentNode.java
+++ trunk/BIOSIM/src/com/traclabs/biosim/editor/graph/environment/SimEnvironmentNode.java
@@ -14,6 +14,12 @@ import com.traclabs.biosim.idl.simulation.environment.O2AirConsumerOperations;
 import com.traclabs.biosim.idl.simulation.environment.O2AirProducerOperations;
 import com.traclabs.biosim.idl.simulation.environment.WaterAirConsumerOperations;
 import com.traclabs.biosim.idl.simulation.environment.WaterAirProducerOperations;
+import com.traclabs.biosim.server.sensor.environment.CO2AirPressureSensorImpl;
+import com.traclabs.biosim.server.sensor.environment.EnvironmentSensorImpl;
+import com.traclabs.biosim.server.sensor.environment.NitrogenAirPressureSensorImpl;
+import com.traclabs.biosim.server.sensor.environment.O2AirPressureSensorImpl;
+import com.traclabs.biosim.server.sensor.environment.OtherAirPressureSensorImpl;
+import com.traclabs.biosim.server.sensor.environment.WaterAirPressureSensorImpl;
 import com.traclabs.biosim.server.simulation.environment.SimEnvironmentImpl;
 import com.traclabs.biosim.server.simulation.framework.SimBioModuleImpl;
 
@@ -21,6 +27,12 @@ import com.traclabs.biosim.server.simulation.framework.SimBioModuleImpl;
 public class SimEnvironmentNode extends PassiveNode{
     private SimEnvironmentImpl mySimEnvironmentImpl;
     private static int nameID = 0;
+
+    private EnvironmentSensorImpl myO2AirPressureSensorImpl;
+    private EnvironmentSensorImpl myCO2AirPressureSensorImpl;
+    private EnvironmentSensorImpl myNitrogenAirPressureSensorImpl;
+    private EnvironmentSensorImpl myOtherAirPressureSensorImpl;
+    private EnvironmentSensorImpl myWaterAirPressureSensorImpl;
     
     private final static Class[] myProducersAllowed = {AirProducerOperations.class, CO2AirProducerOperations.class, O2AirProducerOperations.class, NitrogenAirProducerOperations.class, WaterAirProducerOperations.class};
     private final static Class[] myConsumersAllowed = {AirConsumerOperations.class, CO2AirConsumerOperations.class, O2AirConsumerOperations.class, NitrogenAirConsumerOperations.class, WaterAirConsumerOperations.class};
@@ -51,5 +63,25 @@ public class SimEnvironmentNode extends PassiveNode{
      */
     public Class[] getConsumersAllowed() {
         return myConsumersAllowed;
+    }
+    
+    public void addSensor() {
+        myO2AirPressureSensorImpl =  new O2AirPressureSensorImpl(0, getSimBioModuleImpl().getModuleName() + "O2AirPressureSensor");
+        myCO2AirPressureSensorImpl =  new CO2AirPressureSensorImpl(0, getSimBioModuleImpl().getModuleName() + "CO2AirPressureSensor");
+        myNitrogenAirPressureSensorImpl =  new NitrogenAirPressureSensorImpl(0, getSimBioModuleImpl().getModuleName() + "NitrogenAirPressureSensor");
+        myOtherAirPressureSensorImpl =  new OtherAirPressureSensorImpl(0, getSimBioModuleImpl().getModuleName() + "OtherAirPressureSensor");
+        myWaterAirPressureSensorImpl =  new WaterAirPressureSensorImpl(0, getSimBioModuleImpl().getModuleName() + "WaterAirPressureSensor");
+    }
+
+    /**
+     * 
+     */
+    public void removeSensor() {
+        myO2AirPressureSensorImpl =  null;
+        myCO2AirPressureSensorImpl =  null;
+        myNitrogenAirPressureSensorImpl = null;
+        myOtherAirPressureSensorImpl =  null;
+        myWaterAirPressureSensorImpl= null;;
+    
     }
 }
