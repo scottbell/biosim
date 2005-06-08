368e674e61f03fd4847d4f6204070f63a6164ee8
scott <scottbell@users.noreply.github.com>
2005-06-08 23:08:16 +0000

    Placeholders for photosynthesis

---
+++ BIOSIM/src/com/traclabs/biosim/server/simulation/food/photosynthesis/Lumen.java
@@ -0,0 +1,21 @@
+/*
+ * Created on Jun 8, 2005
+ *
+ * TODO
+ */
+package com.traclabs.biosim.server.simulation.food.photosynthesis;
+
+/**
+ * @author scott
+ *
+ * TODO
+ */
+public class Lumen {
+    
+    private float myWaterConcentrationCached;
+    private float myHydrogenConcentrationCached;
+
+    private float myWaterConcentrationCurrent;
+    private float myHydrogenConcentrationCurrent;
+
+}
