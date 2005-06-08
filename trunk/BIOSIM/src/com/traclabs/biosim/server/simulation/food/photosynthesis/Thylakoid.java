4fd5f50293ccd313d0d6d2967a31470b7aa61421
scott <scott@4576cf88-1eed-0310-b270-a9f98363dfe6>
2005-06-08 23:08:16 +0000

    Placeholders for photosynthesis

---
+++ trunk/BIOSIM/src/com/traclabs/biosim/server/simulation/food/photosynthesis/Thylakoid.java
@@ -0,0 +1,22 @@
+/*
+ * Created on Jun 8, 2005
+ *
+ * TODO
+ */
+package com.traclabs.biosim.server.simulation.food.photosynthesis;
+
+/**
+ * @author scott
+ *
+ * TODO
+ */
+public class Thylakoid {
+    private Photosystem1 myPhotosystem1;
+    private Photosystem2 myPhotosystem2;
+    private Plastoquinone myPlastoquinone;
+    private CytochromeB6F myCytochromeB6F;
+    private Plastocyanin myPlastocyanin;
+    private Ferredoxin myFerredoxin;
+    private FNR myFNR;
+    private Lumen myLumen;
+}
