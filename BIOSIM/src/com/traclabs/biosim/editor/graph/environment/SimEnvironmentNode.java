296b9ac18c9d2efeb6df6b58fd2152e6a3c8da6c
scott <scottbell@users.noreply.github.com>
2005-06-30 16:59:15 +0000

    Accumulatorr/Injectors work as planned!

--- BIOSIM/src/com/traclabs/biosim/editor/graph/environment/SimEnvironmentNode.java
+++ BIOSIM/src/com/traclabs/biosim/editor/graph/environment/SimEnvironmentNode.java
@@ -4,6 +4,12 @@ import org.tigris.gef.base.Layer;
 import org.tigris.gef.presentation.FigNode;
 
 import com.traclabs.biosim.editor.graph.PassiveNode;
+import com.traclabs.biosim.idl.simulation.air.CO2ConsumerOperations;
+import com.traclabs.biosim.idl.simulation.air.CO2ProducerOperations;
+import com.traclabs.biosim.idl.simulation.air.NitrogenConsumerOperations;
+import com.traclabs.biosim.idl.simulation.air.NitrogenProducerOperations;
+import com.traclabs.biosim.idl.simulation.air.O2ConsumerOperations;
+import com.traclabs.biosim.idl.simulation.air.O2ProducerOperations;
 import com.traclabs.biosim.idl.simulation.environment.AirConsumerOperations;
 import com.traclabs.biosim.idl.simulation.environment.AirProducerOperations;
 import com.traclabs.biosim.server.simulation.environment.SimEnvironmentImpl;
@@ -14,8 +20,8 @@ public class SimEnvironmentNode extends PassiveNode{
     private SimEnvironmentImpl mySimEnvironmentImpl;
     private static int nameID = 0;
     
-    private final static Class[] myProducersAllowed = {AirProducerOperations.class};
-    private final static Class[] myConsumersAllowed = {AirConsumerOperations.class};
+    private final static Class[] myProducersAllowed = {AirProducerOperations.class, O2ProducerOperations.class, CO2ProducerOperations.class, NitrogenProducerOperations.class};
+    private final static Class[] myConsumersAllowed = {AirConsumerOperations.class, O2ConsumerOperations.class, CO2ConsumerOperations.class, NitrogenConsumerOperations.class};
     
     public SimEnvironmentNode() {
         mySimEnvironmentImpl = new SimEnvironmentImpl(0, "SimEnvironment"+nameID++);
