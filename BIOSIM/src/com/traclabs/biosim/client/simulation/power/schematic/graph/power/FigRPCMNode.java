55cab6ac643b8830ff54018c5beedc986dd5a3c6
scott <scottbell@users.noreply.github.com>
2006-01-23 01:37:20 +0000

    Rudimentary RPCM properties implemented

--- BIOSIM/src/com/traclabs/biosim/client/simulation/power/schematic/graph/power/FigRPCMNode.java
+++ BIOSIM/src/com/traclabs/biosim/client/simulation/power/schematic/graph/power/FigRPCMNode.java
@@ -2,21 +2,34 @@ package com.traclabs.biosim.client.simulation.power.schematic.graph.power;
 
 import java.awt.Color;
 
+import javax.swing.JFrame;
+
 import com.traclabs.biosim.client.simulation.power.schematic.graph.FigActiveNode;
+import com.traclabs.biosim.client.simulation.power.schematic.ui.RPCMPropertiesFrame;
 import com.traclabs.biosim.idl.simulation.power.RPCM;
 
 public class FigRPCMNode extends FigActiveNode {
+	private JFrame myEditFrame;
+	
     public FigRPCMNode() {
         super();
         setFillColor(Color.LIGHT_GRAY);
         setLineColor(Color.BLACK);
+    }
+    
+    protected JFrame getPropertiesEditor() {
+        if (myEditFrame != null)
+            return myEditFrame;
+        myEditFrame = new RPCMPropertiesFrame(this);
+        myEditFrame.pack();
+        return myEditFrame;
     }
 
 	@Override
 	public void refresh() {
 		RPCMNode rpcmNode = (RPCMNode)getOwner();
 		RPCM myRPCM = (RPCM)rpcmNode.getSimBioModule();
-		if (myRPCM.isOverTripped() || myRPCM.isUnderTripped()){
+		if (myRPCM.isOvertripped() || myRPCM.isUndertripped()){
 			setFillColor(computeColorOnPercentage(0));
 			myLogger.debug("percentage is 0");
 		}
