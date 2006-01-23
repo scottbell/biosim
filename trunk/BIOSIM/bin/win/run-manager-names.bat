f076918575bdec0e6471b7d2efc8f37915acd494
scott <scott@4576cf88-1eed-0310-b270-a9f98363dfe6>
2006-01-23 02:02:51 +0000

    Problems after clearing power.

--- trunk/BIOSIM/bin/win/run-manager-names.bat
+++ trunk/BIOSIM/bin/win/run-manager-names.bat
@@ -5,5 +5,5 @@ set jacoSingletonOrbClass=org.omg.CORBA.ORBSingletonClass=org.jacorb.orb.ORBSing
 set jacoNameIOR="ORBInitRef.NameService=file:%BIOSIM_HOME%\tmp\ns\ior.txt"
 set resourceDir="%BIOSIM_HOME%\resources"
 set jacoClasspath="%BIOSIM_HOME%\lib\jacorb\jacorb.jar;%BIOSIM_HOME%\lib\jacorb;%BIOSIM_HOME%\lib\jacorb\avalon-framework.jar;%BIOSIM_HOME%\lib\jacorb\logkit.jar"
-java -classpath %jacoClasspath% -D%jacoOrbClass% -D%jacoSingletonOrbClass% -D%jacoNameIOR% "%mainClass%"
+java -classpath %jacoClasspath% -D%jacoOrbClass% -D%jacoSingletonOrbClass% -D%jacoNameIOR% "%mainClass%" %1 %2 %3 %4 %5 %6 %7 %8 %9
 pause
