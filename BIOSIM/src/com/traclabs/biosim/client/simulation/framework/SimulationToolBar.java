b0ed3971e9c691f52a21f51eef7ba541fb38754d
scott <scottbell@users.noreply.github.com>
2006-01-23 17:55:17 +0000

    Made toolbar better.

--- BIOSIM/src/com/traclabs/biosim/client/simulation/framework/SimulationToolBar.java
+++ BIOSIM/src/com/traclabs/biosim/client/simulation/framework/SimulationToolBar.java
@@ -7,135 +7,206 @@ import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JOptionPane;
 import javax.swing.JToolBar;
 
 import com.traclabs.biosim.client.util.BioHolderInitializer;
-import com.traclabs.biosim.client.util.Fnorder;
 import com.traclabs.biosim.idl.framework.BioDriver;
 
 public class SimulationToolBar extends JToolBar {
-	private JButton myResetButton;
 
-	private final static String myResetToolTipText = "Reset the simulation.";
+	private BioDriver myDriver;
 
-	private JButton myTickButton;
+	private JButton myStartSimButton;
 
-	private final static String myTickToolTipText = "Advance the simulation one tick";
+	private JButton myPauseSimButton;
 
-	private JButton myPlayPauseButton;
-
-	private final static String myPlayToolTipText = "Run the simulation";
-
-	private final static String myPauseToolTipText = "Pause the simulation";
-
-	private BioDriver myBioDriver;
+	private JButton myAdvanceSimButton;
 
 	// Icons
-	private ImageIcon myResetIcon;
+	private ImageIcon startIcon;
 
-	private ImageIcon myPlayIcon;
+	private ImageIcon playIcon;
 
-	private ImageIcon myPauseIcon;
+	private ImageIcon stopIcon;
 
-	private ImageIcon myTickIcon;
+	private ImageIcon pauseIcon;
 
-	private Action myPlayPauseButtonAction;
+	private ImageIcon forwardIcon;
+
+	private Action myStartAction;
+
+	private Action myPauseAction;
+
+	private Action myAdvanceAction;
 
 	public SimulationToolBar() {
-		myBioDriver = BioHolderInitializer.getBioHolder().theBioDriver;
+		myDriver = BioHolderInitializer.getBioHolder().theBioDriver;
 		buildGUI();
 	}
 
 	private void buildGUI() {
 		loadIcons();
-		myResetButton = new JButton(new ResetButtonAction());
-		myResetButton.setIcon(myResetIcon);
-		myResetButton.setToolTipText(myPlayToolTipText);
-		myTickButton = new JButton(new TickButtonAction());
-		myTickButton.setIcon(myTickIcon);
-		myTickButton.setToolTipText(myTickToolTipText);
-		myPlayPauseButtonAction = new PlayPauseButtonAction();
-		myPlayPauseButton = new JButton(myPlayPauseButtonAction);
-		myPlayPauseButton.setIcon(myPlayIcon);
-		myPlayPauseButton.setToolTipText(myPlayToolTipText);
-		add(myResetButton);
-		add(myPlayPauseButton);
-		add(myTickButton);
+		myStartAction = new StartSimulationAction("Start");
+		myPauseAction = new PauseSimulationAction("Pause");
+		myAdvanceAction = new AdvanceTickSimulationAction("Advance");
+
+		myStartSimButton = add(myStartAction);
+		myStartSimButton.setToolTipText("Starts the simulation");
+		myStartSimButton.setText("");
+		myStartSimButton.setIcon(startIcon);
+		myStartSimButton.setEnabled(false);
+		myPauseSimButton = add(myPauseAction);
+		myPauseSimButton.setToolTipText("Pauses the simulation");
+		myPauseSimButton.setIcon(pauseIcon);
+		myPauseSimButton.setText("");
+		myPauseSimButton.setEnabled(true);
+		myAdvanceSimButton = add(myAdvanceAction);
+		myAdvanceSimButton
+				.setToolTipText("Advances the simulation one timestep");
+		myAdvanceSimButton.setText("");
+		myAdvanceSimButton.setIcon(forwardIcon);
+		myAdvanceSimButton.setEnabled(false);
+
 		refresh();
 	}
 
 	private void loadIcons() {
-		myResetIcon = new ImageIcon(SimulationPanel.class.getClassLoader()
+		startIcon = new ImageIcon(SimDesktop.class.getClassLoader()
 				.getResource("com/traclabs/biosim/client/framework/start.png"));
-
-		myTickIcon = new ImageIcon(
-				SimulationPanel.class.getClassLoader().getResource(
-						"com/traclabs/biosim/client/framework/forward.png"));
-		myPlayIcon = new ImageIcon(SimulationPanel.class.getClassLoader()
-				.getResource("com/traclabs/biosim/client/framework/play.png"));
-
-		myPauseIcon = new ImageIcon(SimulationPanel.class.getClassLoader()
+		playIcon = new ImageIcon(SimDesktop.class.getClassLoader().getResource(
+				"com/traclabs/biosim/client/framework/play.png"));
+		stopIcon = new ImageIcon(SimDesktop.class.getClassLoader().getResource(
+				"com/traclabs/biosim/client/framework/stop.png"));
+		pauseIcon = new ImageIcon(SimDesktop.class.getClassLoader()
 				.getResource("com/traclabs/biosim/client/framework/pause.png"));
-	}
-
-	private class PlayPauseButtonAction extends AbstractAction {
-		public void actionPerformed(ActionEvent ae) {
-			getTopLevelAncestor().setCursor(
-					Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
-			myBioDriver.setPauseSimulation(!myBioDriver.isPaused());
-			getTopLevelAncestor().setCursor(Cursor.getDefaultCursor());
-			refresh();
-		}
-	}
-
-	private class TickButtonAction extends AbstractAction {
-		public void actionPerformed(ActionEvent ae) {
-			getTopLevelAncestor().setCursor(
-					Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
-			myBioDriver.advanceOneTick();
-			if (ae.getModifiers() == (ActionEvent.CTRL_MASK + 16)) {
-				Fnorder myFnord = new Fnorder();
-				String message = myFnord.getFnord();
-				ImageIcon fnordIcon = new ImageIcon(
-						SimulationPanel.class
-								.getClassLoader()
-								.getResource(
-										"com/traclabs/biosim/client/framework/pyramid.png"));
-				JOptionPane fnordPane = new JOptionPane(message,
-						JOptionPane.INFORMATION_MESSAGE,
-						JOptionPane.DEFAULT_OPTION, fnordIcon);
-				JDialog dialog = fnordPane.createDialog(null,
-						"Your message from the Illuminati");
-				dialog.setVisible(true);
-			}
-			getTopLevelAncestor().setCursor(Cursor.getDefaultCursor());
-		}
-	}
-
-	private class ResetButtonAction extends AbstractAction {
-		public void actionPerformed(ActionEvent ae) {
-			getTopLevelAncestor().setCursor(
-					Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
-			if(!myBioDriver.isStarted())
-						myBioDriver.startSimulation();
-			else
-				myBioDriver.reset();
-			getTopLevelAncestor().setCursor(Cursor.getDefaultCursor());
-		}
+		forwardIcon = new ImageIcon(
+				SimDesktop.class.getClassLoader().getResource(
+						"com/traclabs/biosim/client/framework/forward.png"));
 	}
 
 	public void refresh() {
+		// Simulation has stopped
+		if (!myDriver.isStarted()) {
+			myStartSimButton.setToolTipText("Starts the simulation");
+			myStartSimButton.setIcon(startIcon);
+			myStartSimButton.setEnabled(true);
+			myAdvanceSimButton.setEnabled(false);
+		}
+		// Simulation has started
+		else {
+			myStartSimButton.setToolTipText("Ends the simulation");
+			myStartSimButton.setIcon(stopIcon);
+			myStartSimButton.setEnabled(true);
+			myAdvanceSimButton.setEnabled(true);
+		}
 		// Simulation has paused
-		if (myBioDriver.isPaused()) {
-			myPlayPauseButton.setIcon(myPlayIcon);
-			myPlayPauseButton.setToolTipText(myPlayToolTipText);
+		if (myDriver.isPaused()) {
+			myPauseSimButton.setToolTipText("Resume the simulation");
+			myPauseSimButton.setIcon(playIcon);
+			if (myDriver.isStarted()) {
+				myAdvanceSimButton.setEnabled(true);
+			}
 		}
 		// Simulation has resumed
 		else {
-			myPlayPauseButton.setIcon(myPauseIcon);
-			myPlayPauseButton.setToolTipText(myPauseToolTipText);
+			myPauseSimButton.setToolTipText("Pause the simulation");
+			myPauseSimButton.setIcon(pauseIcon);
+			myAdvanceSimButton.setEnabled(false);
 		}
 	}
+	
+    /**
+     * Action describing a simulation start/stop (depending on the button
+     * state). Enables/disables various buttons on the SimDesktop and invokes
+     * BioSimulator's stop or start method.
+     */
+    private class StartSimulationAction extends AbstractAction {
+        public StartSimulationAction(String name) {
+            super(name);
+        }
+
+        public void actionPerformed(ActionEvent ae) {
+            //We want to stop the simulation
+            if (myDriver.isStarted()) {
+    			getTopLevelAncestor().setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+                myStartSimButton.setToolTipText("Starts the simulation");
+                myStartSimButton.setIcon(startIcon);
+                myAdvanceSimButton.setEnabled(false);
+                myDriver.endSimulation();
+    			getTopLevelAncestor().setCursor(Cursor.getDefaultCursor());
+            }
+            //We want to start the simulation
+            else {
+            	getTopLevelAncestor().setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+                myStartSimButton.setToolTipText("Ends the simulation");
+                myStartSimButton.setIcon(stopIcon);
+                myAdvanceSimButton.setEnabled(true);
+                //If we're paused, stay paused.
+                if (myDriver.isPaused()) {
+                    myPauseSimButton.setToolTipText("Resume the simulation");
+                    myPauseSimButton.setIcon(playIcon);
+                    myAdvanceSimButton.setEnabled(true);
+                }
+                //If we're not paused, start ticking immediatly
+                else {
+                    myPauseSimButton.setToolTipText("Pause the simulation");
+                    myPauseSimButton.setIcon(pauseIcon);
+                    myAdvanceSimButton.setEnabled(false);
+                }
+                //Tell the biosimulator to enter loop
+                myDriver.startSimulation();
+                getTopLevelAncestor().setCursor(Cursor.getDefaultCursor());
+                
+            }
+        }
+    }
+
+    /**
+     * Action describing a simulation pause/resume (depending on the button
+     * state). Enables/disables various buttons on the SimDesktop and invokes
+     * BioSimulator's pause or resume method.
+     */
+    private class PauseSimulationAction extends AbstractAction {
+        public PauseSimulationAction(String name) {
+            super(name);
+        }
+
+        public void actionPerformed(ActionEvent ae) {
+        	getTopLevelAncestor().setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+            //We want to resume the simulation
+            if (myDriver.isPaused()) {
+                myPauseSimButton.setToolTipText("Pause the simulation");
+                myPauseSimButton.setIcon(pauseIcon);
+                myAdvanceSimButton.setEnabled(false);
+                myDriver.setPauseSimulation(false);
+            }
+            //We want to pause the simulation
+            else {
+                myPauseSimButton.setToolTipText("Resume the simulation");
+                myPauseSimButton.setIcon(playIcon);
+                if (myDriver.isStarted()) {
+                    myAdvanceSimButton.setEnabled(true);
+                }
+                myDriver.setPauseSimulation(true);
+            }
+            getTopLevelAncestor().setCursor(Cursor.getDefaultCursor());
+        }
+    }
+
+    /**
+     * Action describing a simulation advance of one tick. Enables/disables
+     * various buttons on the SimDesktop and invokes BioSimulator's tick method.
+     * Used only when BioSimulator is paused.
+     */
+    private class AdvanceTickSimulationAction extends AbstractAction {
+        public AdvanceTickSimulationAction(String name) {
+            super(name);
+        }
+
+        public void actionPerformed(ActionEvent ae) {
+        	getTopLevelAncestor().setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+            myDriver.advanceOneTick();
+            getTopLevelAncestor().setCursor(Cursor.getDefaultCursor());
+        }
+    }
 }
