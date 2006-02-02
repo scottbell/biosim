7e15251c2dce48df1d062e78815934934d5dba12
scott <scott@4576cf88-1eed-0310-b270-a9f98363dfe6>
2006-02-02 20:16:38 +0000

    removing illinois branch

--- branches/illinois/BIOSIM-illinois/src/com/traclabs/biosim/server/simulation/air/vccr/CO2Bed.java
+++
@@ -1,5 +0,0 @@
-package com.traclabs.biosim.server.simulation.air.vccr;
-
-public class CO2Bed extends VCCRSubsystem {
-
-}
--- branches/illinois/BIOSIM-illinois/src/com/traclabs/biosim/server/simulation/air/vccr/Pump.java
+++
@@ -1,9 +0,0 @@
-package com.traclabs.biosim.server.simulation.air.vccr;
-
-public class Pump extends VCCRSubsystem {
-	public void tick(){
-		super.tick();
-		//TODO force gas into higher pressure attachments
-	}
-
-}
