09da487d9bb33d25aac9f0331702eb8df80a024e
scott <scott@4576cf88-1eed-0310-b270-a9f98363dfe6>
2006-04-20 20:16:32 +0000

    removed illinois branch

--- branches/BIOSIM-illinois/bin/win/run-biosim-demo.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-start /B run-nameserver.bat
-start /B run-server.bat -demo
-start /B run-client.bat
--- branches/BIOSIM-illinois/bin/win/run-biosim.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-start /B run-nameserver.bat
-start /B run-server.bat
-start /B run-client.bat
