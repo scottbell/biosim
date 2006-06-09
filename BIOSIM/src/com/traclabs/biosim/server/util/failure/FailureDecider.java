3303d74f155bff3235514df2cbe3015be54a21d9
scott <scottbell@users.noreply.github.com>
2006-06-09 19:42:24 +0000

    First implementation of failure modes that work.

--- BIOSIM/src/com/traclabs/biosim/server/util/failure/FailureDecider.java
+++ BIOSIM/src/com/traclabs/biosim/server/util/failure/FailureDecider.java
@@ -2,17 +2,25 @@ package com.traclabs.biosim.server.util.failure;
 
 import java.util.Random;
 
+import org.apache.log4j.Logger;
+
 public abstract class FailureDecider {
     private Random myRandomGenerator;
+    private Logger myLogger;
     
 	public FailureDecider(){
 		myRandomGenerator = new Random();
+		myLogger = Logger.getLogger(FailureDecider.class);
 	}
 	
 	public boolean hasFailed(double timeElapsed){
 		double failureRate = getFailureRate(timeElapsed);
 		double randomNumber = myRandomGenerator.nextDouble();
-        return (failureRate <= randomNumber);
+		boolean failed = randomNumber <= failureRate;
+		myLogger.debug("failureRate = "+failureRate);
+		myLogger.debug("randomNumber = "+randomNumber);
+		myLogger.debug("failed = "+failed);
+        return failed;
 	}
 	
 	protected abstract double getFailureRate(double timeElapsed);
