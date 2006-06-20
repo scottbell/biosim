27f17e014ac868d9512c61f259752101095e6289
scott <scottbell@users.noreply.github.com>
2006-06-20 20:53:03 +0000

    Cleaned up the code a little bit.

--- BIOSIM/src/com/traclabs/biosim/server/util/failure/ExpDecider.java
+++ BIOSIM/src/com/traclabs/biosim/server/util/failure/ExpDecider.java
@@ -1,47 +1,45 @@
 package com.traclabs.biosim.server.util.failure;
 
-  public class ExpDecider extends FailureDecider{
+public class ExpDecider extends FailureDecider {
 	private double myLambda;
-	
-	public ExpDecider(double lambda){
+
+	public ExpDecider(double lambda) {
 		this.myLambda = lambda;
 	}
-	
-        @Override
+
+	@Override
 	protected double getFailureRate(double timeElapsed) {
 		return expFailureRate(myLambda, timeElapsed);
 	}
-	
+
 	public static double expdensity(double lambda, double x) {
 
-	      double f;
+		double f;
 
-	      f = lambda * Math.exp(lambda*x);
+		f = lambda * Math.exp(lambda * x);
 
-	      return f;
-
-	   }
-
-	 public static double exprel(double lambda, double x) {
-
-	      double R;
-
-		R=Math.exp(lambda*x);
-
-	      return R;
-	   
-	   }
-
-     public static double expFailureRate(double lambda,  double x) {
-
-	      double Z;
-
-		  Z=lambda;
-
-	      return Z;
-
-	  }
+		return f;
 
 	}
 
+	public static double exprel(double lambda, double x) {
 
+		double R;
+
+		R = Math.exp(lambda * x);
+
+		return R;
+
+	}
+
+	public static double expFailureRate(double lambda, double x) {
+
+		double Z;
+
+		Z = lambda;
+
+		return Z;
+
+	}
+
+}
