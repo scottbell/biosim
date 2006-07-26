b47b9de3612db27296ad654218610d189161f332
scott <scott@4576cf88-1eed-0310-b270-a9f98363dfe6>
2006-07-26 20:05:03 +0000

    Unit tests assert that it works.

--- trunk/BIOSIM/src/com/traclabs/biosim/server/simulation/environment/AirConsumerDefinitionImpl.java
+++ trunk/BIOSIM/src/com/traclabs/biosim/server/simulation/environment/AirConsumerDefinitionImpl.java
@@ -1,9 +1,11 @@
 package com.traclabs.biosim.server.simulation.environment;
 
+import com.traclabs.biosim.idl.simulation.environment.Air;
 import com.traclabs.biosim.idl.simulation.environment.AirConsumerDefinition;
 import com.traclabs.biosim.idl.simulation.environment.AirConsumerDefinitionOperations;
 import com.traclabs.biosim.idl.simulation.environment.AirConsumerDefinitionPOATie;
 import com.traclabs.biosim.idl.simulation.environment.SimEnvironment;
+import com.traclabs.biosim.idl.simulation.framework.Store;
 import com.traclabs.biosim.util.OrbUtils;
 
 /**
@@ -31,4 +33,32 @@ public class AirConsumerDefinitionImpl extends
         setInitialMaxFlowRates(pMaxFlowRates);
         setInitialDesiredFlowRates(pDesiredFlowRates);
     }
+
+	public Air getMostAirFromEnvironments() {
+		Air airToReturn = new Air();
+		for (int i = 0; i < getEnvironments().length; i++){
+			SimEnvironment environment = getEnvironments()[i];
+			float airMolesToTake = Math.min(getMaxFlowRate(i), getDesiredFlowRate(i));
+			float molesTakenForThisFlowrate = 0f;
+			molesTakenForThisFlowrate += airToReturn.o2Moles += takeGasFromAir(airMolesToTake, environment.getO2Store(), environment);
+			molesTakenForThisFlowrate += airToReturn.co2Moles += takeGasFromAir(airMolesToTake, environment.getCO2Store(), environment);
+			molesTakenForThisFlowrate += airToReturn.otherMoles += takeGasFromAir(airMolesToTake, environment.getOtherStore(), environment);
+			molesTakenForThisFlowrate += airToReturn.vaporMoles += takeGasFromAir(airMolesToTake, environment.getVaporStore(), environment);
+			molesTakenForThisFlowrate += airToReturn.nitrogenMoles += takeGasFromAir(airMolesToTake, environment.getNitrogenStore(), environment);
+			setActualFlowRate(molesTakenForThisFlowrate, i);
+		}
+		return airToReturn;
+	}
+	
+	private static float calculateTotalAirMoles(Air air){
+		return air.o2Moles + air.co2Moles + air.nitrogenMoles + air.otherMoles + air.vaporMoles;
+	}
+
+	private static float takeGasFromAir(float airMolesToTake, Store store, SimEnvironment environment) {
+		if (environment.getTotalMoles() == 0)
+			return 0f;
+		float gasPercentage = store.getCurrentLevel() / environment.getTotalMoles();
+		float gasMolesToTake = airMolesToTake * gasPercentage;
+		return store.take(gasMolesToTake);
+	}
 }
--- trunk/BIOSIM/src/com/traclabs/biosim/server/simulation/environment/AirProducerDefinitionImpl.java
+++ trunk/BIOSIM/src/com/traclabs/biosim/server/simulation/environment/AirProducerDefinitionImpl.java
@@ -1,5 +1,6 @@
 package com.traclabs.biosim.server.simulation.environment;
 
+import com.traclabs.biosim.idl.simulation.environment.Air;
 import com.traclabs.biosim.idl.simulation.environment.AirProducerDefinition;
 import com.traclabs.biosim.idl.simulation.environment.AirProducerDefinitionOperations;
 import com.traclabs.biosim.idl.simulation.environment.AirProducerDefinitionPOATie;
@@ -31,4 +32,20 @@ public class AirProducerDefinitionImpl extends
         setInitialMaxFlowRates(pMaxFlowRates);
         setInitialDesiredFlowRates(pDesiredFlowRates);
     }
+	
+	public void pushAirToEnvironment(Air airToPush, int indexOfEnvironment){
+		float actualFlowrateToEnvironment = 0f;
+		SimEnvironment environment = getEnvironments()[indexOfEnvironment];
+		actualFlowrateToEnvironment += environment.getO2Store().add(airToPush.o2Moles);
+		actualFlowrateToEnvironment += environment.getCO2Store().add(airToPush.co2Moles);
+		actualFlowrateToEnvironment += environment.getOtherStore().add(airToPush.otherMoles);
+		actualFlowrateToEnvironment += environment.getVaporStore().add(airToPush.vaporMoles);
+		actualFlowrateToEnvironment += environment.getNitrogenStore().add(airToPush.nitrogenMoles);
+		setActualFlowRate(actualFlowrateToEnvironment, indexOfEnvironment);
+	}
+	
+	
+	private static float calculateTotalAirMoles(Air air){
+		return air.o2Moles + air.co2Moles + air.nitrogenMoles + air.otherMoles + air.vaporMoles;
+	}
 }
