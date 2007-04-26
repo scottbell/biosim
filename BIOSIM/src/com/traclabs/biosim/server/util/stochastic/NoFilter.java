42d407f15e5e4de2405c8a701d77dc752683a300
scott <scottbell@users.noreply.github.com>
2007-04-26 16:22:01 +0000

    Can configure stochastic aspects from xml.

--- BIOSIM/src/com/traclabs/biosim/server/util/stochastic/NoFilter.java
+++ BIOSIM/src/com/traclabs/biosim/server/util/stochastic/NoFilter.java
@@ -1,7 +1,7 @@
 package com.traclabs.biosim.server.util.stochastic;
 
-public class NoFilter implements StochasticFilter {
-	public float randomFilter(float pValue) {
+public class NoFilter extends StochasticFilter {
+	protected float internalFilter(float pValue) {
 		return pValue;
 	}
 
--- BIOSIM/src/com/traclabs/biosim/server/util/stochastic/NormalFilter.java
+++ BIOSIM/src/com/traclabs/biosim/server/util/stochastic/NormalFilter.java
@@ -2,21 +2,14 @@ package com.traclabs.biosim.server.util.stochastic;
 
 import com.traclabs.biosim.server.util.MathUtils;
 
-public class NormalFilter implements StochasticFilter {
+public class NormalFilter extends StochasticFilter {
 	private double myDeviation;
 	
 	public NormalFilter(double deviation){
 		this.myDeviation = deviation;
 	}
 	
-	/**
-     * Randomizes a number passed through it.
-     * 
-     * @param pValue
-     *            Filters using a gaussian function.
-     * @return the randomized result
-     */
-    public float randomFilter(float pValue) {
+    protected float internalFilter(float pValue) {
         if (myDeviation <= 0)
             return pValue;
         double result = MathUtils.gaussian(pValue, myDeviation);
