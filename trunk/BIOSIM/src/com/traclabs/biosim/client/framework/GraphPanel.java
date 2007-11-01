bffa208a0e588cc940b13fec803b4f56e438159d
scott <scott@4576cf88-1eed-0310-b270-a9f98363dfe6>
2007-11-01 18:53:57 +0000

    Removed default errors.

--- trunk/BIOSIM/src/com/traclabs/biosim/client/framework/GraphPanel.java
+++ trunk/BIOSIM/src/com/traclabs/biosim/client/framework/GraphPanel.java
@@ -17,60 +17,62 @@ import com.traclabs.biosim.idl.framework.BioModule;
  */
 public abstract class GraphPanel extends TimedPanel {
 
-    private JButton trackingButton;
+	private JButton trackingButton;
 
-    protected ChartPanel myChartPanel;
+	protected ChartPanel myChartPanel;
 
-    @SuppressWarnings("unused")
+	@SuppressWarnings("unused")
 	private TrackingAction myTrackingAction;
 
-    /**
-     * Default constructor.
-     */
-    public GraphPanel() {
-        super();
-        buildGui();
-    }
+	/**
+	 * Default constructor.
+	 */
+	public GraphPanel() {
+		super();
+		buildGui();
+	}
 
-    public GraphPanel(BioModule module) {
-        super();
-        initializeDataSources(module);
-        buildGui();
-    }
+	public GraphPanel(BioModule module) {
+		super();
+		initializeDataSources(module);
+		buildGui();
+	}
 
-    protected void buildGui() {
-        createGraph();
-        myTrackingAction = new TrackingAction();
-        GridBagLayout gridbag = new GridBagLayout();
-        GridBagConstraints c = new GridBagConstraints();
-        setLayout(gridbag);
-        c.fill = GridBagConstraints.BOTH;
-        c.gridheight = 1;
-        c.gridwidth = GridBagConstraints.REMAINDER;
-        c.weighty = 1.0;
-        c.weightx = 1.0;
-        gridbag.setConstraints(myChartPanel, c);
-        add(myChartPanel);
-    }
+	protected void buildGui() {
+		createGraph();
+		if (myChartPanel != null) {
+			myTrackingAction = new TrackingAction();
+			GridBagLayout gridbag = new GridBagLayout();
+			GridBagConstraints c = new GridBagConstraints();
+			setLayout(gridbag);
+			c.fill = GridBagConstraints.BOTH;
+			c.gridheight = 1;
+			c.gridwidth = GridBagConstraints.REMAINDER;
+			c.weighty = 1.0;
+			c.weightx = 1.0;
+			gridbag.setConstraints(myChartPanel, c);
+			add(myChartPanel);
+		}
+	}
 
-    protected abstract void createGraph();
+	protected abstract void createGraph();
 
-    protected void initializeDataSources(BioModule module) {
-    }
+	protected void initializeDataSources(BioModule module) {
+	}
 
-    /**
-     * Action that displays the power panel in an internal frame on the desktop.
-     */
-    private class TrackingAction extends AbstractAction {
+	/**
+	 * Action that displays the power panel in an internal frame on the desktop.
+	 */
+	private class TrackingAction extends AbstractAction {
 
 		public void actionPerformed(ActionEvent ae) {
-            boolean newState = !(isTracking());
-            setTracking(newState);
-            if (isTracking()) {
-                trackingButton.setText("Stop Tracking");
-            } else {
-                trackingButton.setText("Start Tracking");
-            }
-        }
-    }
+			boolean newState = !(isTracking());
+			setTracking(newState);
+			if (isTracking()) {
+				trackingButton.setText("Stop Tracking");
+			} else {
+				trackingButton.setText("Start Tracking");
+			}
+		}
+	}
 }
