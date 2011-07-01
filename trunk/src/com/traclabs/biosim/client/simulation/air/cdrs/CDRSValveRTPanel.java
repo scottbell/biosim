c5bdd109f4a93803004fabdb68e7d0da51480491
scott <scott@4576cf88-1eed-0310-b270-a9f98363dfe6>
2011-07-01 22:08:22 +0000

    CDRS Valve status works

--- trunk/src/com/traclabs/biosim/client/simulation/air/cdrs/CDRSValveRTPanel.java
+++ trunk/src/com/traclabs/biosim/client/simulation/air/cdrs/CDRSValveRTPanel.java
@@ -12,11 +12,12 @@ import com.traclabs.biosim.idl.simulation.air.cdrs.CDRSCommandStatus;
 
 public class CDRSValveRTPanel extends GridButtonPanel {
 	
+	private int currentRow = 0;
+	
 	public CDRSValveRTPanel(){
 		setName("CDRS Valve RT Status");
 		GridBagLayout layout = new GridBagLayout();
 		setLayout(layout);
-		GridBagConstraints constraints = new GridBagConstraints();
 		
 		ActionListener ventEnable = new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -25,24 +26,96 @@ public class CDRSValveRTPanel extends GridButtonPanel {
 		};
 		ActionListener ventInhibit = new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				LssmViewer.getCDRSModule().setCO2VentValveArmedStatus(CDRSCommandStatus.enabled);
+				LssmViewer.getCDRSModule().setCO2VentValveArmedStatus(CDRSCommandStatus.inibited);
 			}
 		};
 		StatusLabel ventStatusLabel = new StatusLabel() {
 			public void refresh() {
-				setText(LssmViewer.getCDRSModule().getCO2VentValveArmedStatus().toString());
+				setText(getCDRSCommandStatus(LssmViewer.getCDRSModule().getCO2VentValveArmedStatus()));
 			}
 		};
+		addLine("CO2 Vent Valve", ventEnable, ventInhibit, ventStatusLabel);
 		
+		ActionListener isoEnable = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				LssmViewer.getCDRSModule().setCO2IsolationValveArmedStatus(CDRSCommandStatus.enabled);
+			}
+		};
+		ActionListener isoInhibit = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				LssmViewer.getCDRSModule().setCO2IsolationValveArmedStatus(CDRSCommandStatus.inibited);
+			}
+		};
+		StatusLabel isoStatusLabel = new StatusLabel() {
+			public void refresh() {
+				setText(getCDRSCommandStatus(LssmViewer.getCDRSModule().getCO2IsolationValveArmedStatus()));
+			}
+		};
+		addLine("CO2 Isolation Valve", isoEnable, isoInhibit, isoStatusLabel);
 		
+		ActionListener airInletEnable = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				LssmViewer.getCDRSModule().setAirInletValveArmedStatus(CDRSCommandStatus.enabled);
+			}
+		};
+		ActionListener airInletInhibit = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				LssmViewer.getCDRSModule().setAirInletValveArmedStatus(CDRSCommandStatus.inibited);
+			}
+		};
+		StatusLabel airInletStatusLabel = new StatusLabel() {
+			public void refresh() {
+				setText(getCDRSCommandStatus(LssmViewer.getCDRSModule().getAirInletValveArmedStatus()));
+			}
+		};
+		addLine("Air Inlet Valve", airInletEnable, airInletInhibit, airInletStatusLabel);
 		
+		ActionListener airReturnEnable = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				LssmViewer.getCDRSModule().setAirReturnValveArmedStatus(CDRSCommandStatus.enabled);
+			}
+		};
+		ActionListener airReturnInhibit = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				LssmViewer.getCDRSModule().setAirReturnValveArmedStatus(CDRSCommandStatus.inibited);
+			}
+		};
+		StatusLabel airReturnStatusLabel = new StatusLabel() {
+			public void refresh() {
+				setText(getCDRSCommandStatus(LssmViewer.getCDRSModule().getAirReturnValveArmedStatus()));
+			}
+		};
+		addLine("Air Return Valve", airReturnEnable, airReturnInhibit, airReturnStatusLabel);
+	}
+
+	private void addLine(String lineName, ActionListener enableListener, ActionListener inhibitListener, StatusLabel statusLabel) {
+		GridBagConstraints constraints = new GridBagConstraints();
 		constraints.weightx = 0.5;
-		constraints.fill = GridBagConstraints.HORIZONTAL;
-		JLabel co2VentLabel = new JLabel("CO2 Vent Valve");
-		add(co2VentLabel, constraints);
+		constraints.fill = GridBagConstraints.BOTH;
+		constraints.gridx = 0;
+		constraints.gridy = currentRow;
+		JLabel label = new JLabel(lineName);
+		add(label, constraints);
 		
-		JButton co2VentEnableButton = new JButton("Enable");
+		JButton enableButton = new JButton("Enable");
+		enableButton.addActionListener(enableListener);
+		constraints.gridx = 1;
+		constraints.gridy = currentRow;
+		add(enableButton, constraints);
 		
+		JButton inhibitButton = new JButton("Inhibit");
+		inhibitButton.addActionListener(inhibitListener);
+		constraints.gridx = 2;
+		constraints.gridy = currentRow;
+		add(inhibitButton, constraints);
+		
+		addUpdateable(statusLabel);
+		constraints.gridwidth = GridBagConstraints.REMAINDER;
+		constraints.gridx = 3;
+		constraints.gridy = currentRow;
+		add(statusLabel, constraints);
+		
+		currentRow++;
 	}
 	
 }
